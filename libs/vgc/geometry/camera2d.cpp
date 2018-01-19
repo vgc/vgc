b4e76aefa81890afb38530f8dd076926008b0a70
Boris Dalstein <dalboris@gmail.com>
2018-01-19 20:16:22 +0100

    Started implementation of Camera2d::viewMatrix() and projectionMatric(). Only support translation for now.

--- libs/vgc/geometry/camera2d.cpp
+++ libs/vgc/geometry/camera2d.cpp
@@ -15,9 +15,52 @@
 // limitations under the License.
 
 #include <vgc/geometry/camera2d.h>
+#include <cmath>
 
 namespace vgc {
 namespace geometry {
+
+Camera2d::Camera2d() :
+    center_(0, 0),
+    zoom_(1),
+    rotation_(0),
+    viewportWidth_(1),
+    viewportHeight_(1),
+    near_(-1),
+    far_(1)
+{
+
+}
+
+Mat4d Camera2d::viewMatrix() const
+{
+    // XXX For now, only support translation for quick testing
+
+    const double cx = center_.x();
+    const double cy = center_.y();
+    const double w = viewportWidth_;
+    const double h = viewportHeight_;
+
+    return Mat4d(1, 0, 0, w/2 - cx,
+                 0, 1, 0, h/2 - cy,
+                 0, 0, 1, 0,
+                 0, 0, 0, 1);
+}
+
+Mat4d Camera2d::projectionMatrix() const
+{
+    const double w = viewportWidth_;
+    const double h = viewportHeight_;
+    const double n = near_;
+    const double f = far_;
+
+    return Mat4d(2/w , 0    , 0       , -1          ,
+                 0   , -2/h , 0       , 1           ,  // Inversion of Y axis (SVG -> OpenGL conventions)
+                 0   , 0    , 2/(n-f) , (n+f)/(n-f) ,  // XXX I'm not 100% sure of the signs on this row. Try later: an object at (0, 0, 0.5) should obsure one at (0, 0, -0.5)
+                 0   , 0    , 0       , 1           );
+
+
+}
 
 } // namespace geometry
 } // namespace vgc
--- libs/vgc/geometry/camera2d.h
+++ libs/vgc/geometry/camera2d.h
@@ -18,6 +18,7 @@
 #define VGC_GEOMETRY_CAMERA2D_H
 
 #include <vgc/geometry/api.h>
+#include <vgc/geometry/mat4d.h>
 #include <vgc/geometry/vec2d.h>
 
 namespace vgc {
@@ -26,21 +27,20 @@ namespace geometry {
 /// \class vgc::geometry::Camera2d
 /// \brief 2D camera using double-precision floating points.
 ///
-/// This class is intended to be used to provide mouse navigation (e.g., pan,
-/// zoom, rotate) in a 2D viewer via intuitive controls. In traditional
-/// computer graphics terminology (e.g., OpenGL), this class represents both
-/// the "view" matrix and the "projection" matrix.
+/// This class is intended to be used for mouse navigation (e.g., pan,
+/// zoom, rotate) in a 2D viewer via intuitive controls.
 ///
 /// A 2D camera is defined via the following properties:
 ///
-/// - center: 2D position, in scene coordinates, which appears
-///   at the center of the viewport.
+/// - center: 2D position, in world coordinates, which appears at the center of
+///   the viewport.
 ///
-/// - zoom: ratio between the size of an object in pixels and its size in scene
-///   coordinates. Example: if zoom = 2, then one unit in scene coordinates
-///   appears as 2 pixels on screen.
+/// - zoom: ratio between the size of an object in view coordinates (i.e., in
+///   pixels), and its size in world coordinates. Example: if zoom = 2, then an
+///   object which is 100-unit wide in world coordinates appears as 200 pixels
+///   on screen.
 ///
-/// - rotation: angle, in radian, between scene coordinates and view coordinates.
+/// - rotation: angle, in radian, between world coordinates and view coordinates.
 ///   Example: if angle = pi/4, then objects appear rotated 45 degrees
 ///   anti-clockwise.
 ///
@@ -52,14 +52,87 @@ namespace geometry {
 ///
 /// - far: value above which z-coordinates are clipped. Default is 1.0.
 ///
+/// Where:
+///
+/// - "World coordinates" refer to the coordinates of an object as authored
+///   by the user. For example, in the following example:
+///
+///   <vgc>
+///     <vertex pos="300 100" />,
+///   </vgc>,
+///
+///   the world coordinates of the vertex are (300, 100). Note that the world
+///   coordinates of objects do not change when the user pan, zoom, or rotate
+///   the view.
+///
+///   In SVG terminology, world coordinates are referred to as "user space".
+///   For consistency with SVG, we use the convention that the Y-axis in
+///   world coordinates is top-down:
+///
+///   \verbatim
+///         o---> X
+///         |
+///         v Y
+///   \endverbatim
+///
+/// - "Viewport" refers to the area of the screen where the VGC illustration
+///   or animation is rendered.
+///
+/// - "View coordinates" refer to the coordinates of an object relative to the
+///   viewport. For example, an object which appears exactly at the top-left
+///   corner of the viewport has view coordinates equal to (0, 0). For
+///   consistency with Qt (i.e., widget coordinates), we use the convention that
+///   the viewport origin is top-left, and that the Y-axis is top-down:
+///
+///   \verbatim
+///         o---> X
+///         |
+///         v Y
+///   \endverbatim
+///
+///   Note that the view coordinates of an object change when the user pan,
+///   zoom, or rotate the view. For example, an object A whose world
+///   coordinates are (0, 0) may be initially rendered at the top-left corner
+///   of the viewport, in which case its view coordinates are also (0, 0). But
+///   if the user decides to center the viewport on the world origin, then the
+///   view coordinates of A becomes (w/2, h/2), where (w,h) is the size of the
+///   viewport in pixels.
+///
+/// In order to convert from world coordinates to view coordinates, one can
+/// use the viewMatrix() associated with the 2D camera:
+///
+/// \code
+/// Vec2d viewCoords = camera.viewMatrix() * worldCoords;
+/// \endcode
+///
+/// This view matrix is always invertible, therefore we also have:
+///
+/// \code
+/// Vec2d worldCoords = camera.viewMatrix().inverse() * viewCoords;
+/// \endcode
+///
+/// The projectionMatrix() is provided for conveninence when using OpenGL. It
+/// maps from view coordinates to NDC (normalized device coordinates), that is,
+/// the top-left corner of the viewport (0, 0) is mapped to (-1, -1), and the
+/// bottom
+///
+/// \verbatim
+///       Y
+///    ---^---  OpenGL NDC
+///   |   |   |
+///   |   o--->  X
+///   |       |
+///    -------
+/// \endverbatim
+///
 class VGC_GEOMETRY_API Camera2d
 {
 public:
-    /// Construct a 2D Camera centered at the scene origin, without zoom or rotation.
+    /// Construct a 2D Camera centered at the world origin, without zoom or rotation.
     ///
     Camera2d();
 
-    /// Returns the center of the camera. This is the 2D position, in scene
+    /// Returns the center of the camera. This is the 2D position, in world
     /// coordinates, which appears at the center of the viewport.
     ///
     /// \sa setCenter()
@@ -73,8 +146,9 @@ public:
     void setCenter(const Vec2d& center) { center_ = center; }
 
     /// Returns the zoom of the camera. This is the ratio between the size of
-    /// an object in pixels and its size in scene coordinates. Example: if zoom
-    /// = 2, then one unit in scene coordinates appears as 2 pixels on screen.
+    /// an object in view coordinates (i.e., in pixels), and its size in world
+    /// coordinates. Example: if zoom = 2, then an object which is 100-unit
+    /// wide in world coordinates appears as 200 pixels on screen.
     ///
     /// \sa setZoom()
     ///
@@ -87,7 +161,7 @@ public:
     void setZoom(double zoom) { zoom_ = zoom; }
 
     /// Returns the rotation of the camera. This is the angle, in radian,
-    /// between scene coordinates and view coordinates. Example: if angle =
+    /// between world coordinates and view coordinates. Example: if angle =
     /// pi/4, then objects appear rotated 45 degrees anti-clockwise.
     ///
     /// \sa setRotation()
@@ -110,7 +184,7 @@ public:
     ///
     /// \sa viewportWidth()
     ///
-    void setViewportWidth(double viewportWidth) { viewportWidth_ = viewportWidth; }
+    void setViewportWidth(double width) { viewportWidth_ = width; }
 
     /// Returns the height of the viewport, in pixels.
     ///
@@ -122,7 +196,16 @@ public:
     ///
     /// \sa viewportHeight()
     ///
-    void setViewportHeight(double viewportHeight) { viewportHeight_ = viewportHeight; }
+    void setViewportHeight(double height) { viewportHeight_ = height; }
+
+    /// Sets the viewport size.
+    ///
+    /// \sa viewportWidth(), viewportHeight()
+    ///
+    void setViewportSize(double width, double height) {
+        viewportWidth_ = width;
+        viewportHeight_ = height;
+    }
 
     /// Returns the near value of the camera. This is the value under which
     /// z-coordinates are clipped. Default is -1.0.
@@ -149,6 +232,16 @@ public:
     /// \sa far()
     ///
     void setFar(double far) { far_ = far; }
+
+    /// Returns the 4x4 view matrix corresponding to the camera.
+    ///
+    Mat4d viewMatrix() const;
+
+    /// Returns the 4x4 projection matrix corresponding to the camera.
+    ///
+    Mat4d projectionMatrix() const;
+
+    /// Returns the scene coordinate that corresponds to the given
 
 private:
     Vec2d center_;
