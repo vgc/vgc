9d996144fea1fe7ae53e3683f628ede704376c90
Boris Dalstein <dalboris@gmail.com>
2018-03-09 16:48:46 +0100

    Improve core::Object documentation and declare const pointers

--- libs/vgc/core/object.h
+++ libs/vgc/core/object.h
@@ -20,20 +20,79 @@
 #include <memory>
 #include <vgc/core/api.h>
 
-#define VGC_CORE_DECLARE_PTRS(T)             \
-    class T;                                 \
-    using T##SharedPtr = std::shared_ptr<T>; \
-    using T##WeakPtr = std::weak_ptr<T>
+#define VGC_CORE_DECLARE_PTRS(T)                        \
+    class T;                                            \
+    using T##SharedPtr      = std::shared_ptr<T>;       \
+    using T##WeakPtr        = std::weak_ptr<T>;         \
+    using T##ConstSharedPtr = std::shared_ptr<const T>; \
+    using T##ConstWeakPtr   = std::weak_ptr<const T>
 
 namespace vgc {
 namespace core {
 
 /// \class vgc::core::Object
-/// \brief Object model used in the VGC codebase.
+/// \brief Base class of most classes in the VGC codebase.
 ///
-/// In the VGC codebase, we extensively use std::shared_ptr and std::weak_ptr for
-/// memory management and thread safety. This also allows convenient implementation
-/// of Qt-style signals/slots.
+/// This base class allows the VGC codebase to be more readable, consistent,
+/// maintainable, thread-safe, and Python-binding-friendly at only a very small
+/// performance cost. Classes deriving from Object should be declared as
+/// follows:
+///
+/// \code
+/// class Foo: public vgc::core::Object<Foo>
+/// {
+/// public:
+///
+/// //...
+/// };
+/// \endcode
+///
+/// It is strongly recommended that new classes derive from
+/// Object. In particular any class that contains a std::vector, a std::string,
+/// any other container, or performs some sort of memory allocation (e.g.,
+/// pimpl idiom) should inherit Object or document clearly why they do not.
+/// Notable exceptions include small struct-like classes that do not perform
+/// any dynamic allocation, are expected to be potentially instanciated more
+/// than a million times in a session, and are expected or measured to be a
+/// bottleneck if they would derive from Object (example: Vec2d).
+///
+/// Typically, objects whose class derives from Object should be instanciated
+/// via their make() static method, which return a std::shared_ptr. In most
+/// cases, ownership is not actually "shared", but still we always use
+/// shared_ptr instead of unique_ptr in order to allow observers to hold
+/// weak_ptr to these object, which requires reference counting.
+///
+/// Even though object owners hold shared_ptrs, prefer passing these objects by
+/// raw pointers. This is simpler, more efficient, more readable, and more
+/// flexible. Unless stated otherwise, all functions taking Objects by pointers
+/// make the following assumptions:
+///
+/// 1. The pointer is non-null
+///
+/// 2. The object was created via make(), that is, the function can called
+///    obj->ptr() to retrieve a shared pointer from the raw pointer.
+///
+/// 3. The lifetime of the object exceeds the lifetime of the function.
+///
+/// This follows the "keep it simple" advice from Herb Sutter, and I strongly
+/// encourage any VGC developer to watch this talk before contributing:
+///
+/// https://www.youtube.com/watch?v=xnqTKD8uD64
+///
+/// If a function desires to keep a pointer to the passed object for
+/// observation purposes, then it should hold a weak_ptr to this object. This
+/// way, the lifetime of the observed object is not extended (as it would if
+/// the function would hold a shared_ptr instead), and the function can check
+/// later if the object still exists (which coudn't be done if the function
+/// would hold a raw pointer instead).
+///
+/// If an object is passed to a function by shared_ptr, this typically indicates
+/// that the function takes ownership of the object. In this case, pass the
+/// shared_ptr directly by value, like so:
+///
+/// \code
+/// myFunction(FooSharedPtr foo);
+/// \endcode
 ///
 template <typename T>
 class Object: public std::enable_shared_from_this<T>
