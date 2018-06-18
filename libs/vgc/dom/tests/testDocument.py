caae7d4d824965960707465d66e60ae053a3c784
Boris Dalstein <dalboris@gmail.com>
2018-06-18 15:19:06 +0200

    Add test for dom::Document()

---
+++ libs/vgc/dom/tests/testDocument.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# XXX There is no proper testing architecture implemented yet.
+# In the meantime, just run the test manually via:
+#
+#     cd <vgc-build-dir>
+#     PYTHONPATH=python python3 <vgc-source-dir>/libs/vgc/dom/tests/testDocument.py
+
+from vgc.dom import Document
+
+document = Document()
