87c62b028b478651dc33252f1830a512428bf823
Boris Dalstein <dalboris@gmail.com>
2018-06-20 14:31:19 +0200

    Add Copyright headers to tests and remove now irrelevant header comment

--- libs/vgc/dom/tests/test_document.py
+++ libs/vgc/dom/tests/test_document.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
 
-# XXX There is no proper testing architecture implemented yet.
-# In the meantime, just run the test manually via:
+# Copyright 2018 The VGC Developers
+# See the COPYRIGHT file at the top-level directory of this distribution
+# and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
 #
-#     cd <vgc-build-dir>
-#     PYTHONPATH=python python3 <vgc-source-dir>/libs/vgc/dom/tests/test_document.py
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 import unittest
 
