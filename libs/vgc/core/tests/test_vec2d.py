9dd45fe174a80d246346136ce82bf26f244473be
Boris Dalstein <dalboris@gmail.com>
2018-07-01 19:52:59 +0200

    Wrap and test Vec2d comparison operators; clean/improve other Vec2d tests

--- libs/vgc/core/tests/test_vec2d.py
+++ libs/vgc/core/tests/test_vec2d.py
@@ -36,26 +36,20 @@ class TestVec2d(unittest.TestCase):
     def testCopyByReference(self):
         v1 = Vec2d(12.5, 42)
         v2 = v1
-        self.assertEqual(v2.x, 12.5)
-        self.assertEqual(v2.y, 42)
-        self.assertEqual(v2, v1)
-        self.assertIs(v2, v1)
+        self.assertIs(v1, v2)
+        self.assertEqual(v1, v2)
         v2.x = 15
         self.assertEqual(v1.x, 15)
-        self.assertEqual(v2, v1)
-        self.assertIs(v2, v1)
+        self.assertEqual(v1, v2)
 
     def testCopyByValue(self):
         v1 = Vec2d(12.5, 42)
         v2 = Vec2d(v1)
-        self.assertEqual(v2.x, 12.5)
-        self.assertEqual(v2.y, 42)
-        #self.assertEqual(v2, v1) # XXX Fail for now TODO Wrap equality operator
-        self.assertIsNot(v2, v1)
+        self.assertIsNot(v1, v2)
+        self.assertEqual(v1, v2)
         v2.x = 15
         self.assertEqual(v1.x, 12.5)
-        self.assertNotEqual(v2, v1)
-        self.assertIsNot(v2, v1)
+        self.assertNotEqual(v1, v2)
 
     def testBracketOperator(self):
         v = Vec2d(12.5, 42)
@@ -75,53 +69,76 @@ class TestVec2d(unittest.TestCase):
         self.assertEqual(v.x, 13.5)
         self.assertEqual(v.y, 43)
 
-    def testArithmetic(self):
+    def testArithmeticOperators(self):
         v1 = Vec2d(1, 2)
         v1 += Vec2d(3, 4)
         self.assertEqual(v1.x, 4)
         self.assertEqual(v1.y, 6)
-        #self.assertEqual(v1, Vec2d(4, 6)) XXX fails due to lack of equality operator wrapper
-        # XXX TODO test other operators once equality wrapper is written
+        self.assertEqual(v1, Vec2d(4, 6))
+        v2 = v1 + Vec2d(1, 1)
+        self.assertEqual(v1, Vec2d(4, 6))
+        self.assertEqual(v2, Vec2d(5, 7))
+        v2 -= Vec2d(3, 2)
+        self.assertEqual(v2, Vec2d(2, 5))
+        v1 = v1 - v2;
+        self.assertEqual(v1, Vec2d(2, 1))
+        v1 *= 2
+        self.assertEqual(v1, Vec2d(4, 2))
+        v3 = 3 * v1 * 2
+        self.assertEqual(v3, Vec2d(24, 12))
+        v3 /= 2
+        self.assertEqual(v3, Vec2d(12, 6))
+        v4 = v3 / 12
+        self.assertEqual(v4, Vec2d(1, 0.5))
+
+    def testComparisonOperators(self):
+        v1 = Vec2d(1, 2)
+        v2 = Vec2d(3, 4)
+        self.assertTrue(v1 == v1)
+        self.assertTrue(v1 == Vec2d(1, 2))
+        self.assertTrue(v1 != v2)
+        self.assertTrue(v1 != Vec2d(3, 4))
+        self.assertTrue(v1 < v2)
+        self.assertTrue(v1 < Vec2d(2, 0))
+        self.assertTrue(v1 < Vec2d(1, 3))
+        self.assertTrue(v1 <= v1)
+        self.assertTrue(v1 <= v2)
+        self.assertTrue(v1 <= Vec2d(2, 0))
+        self.assertTrue(v1 <= Vec2d(1, 3))
+        self.assertTrue(v2 > v1)
+        self.assertTrue(v2 > Vec2d(2, 100))
+        self.assertTrue(v2 >= v2)
+        self.assertTrue(v2 >= v1)
+        self.assertTrue(v2 >= Vec2d(2, 100))
 
     def testLength(self):
-        v = Vec2d(3, 4)
-        self.assertEqual(v.length(), 5)
-        self.assertEqual(v.squaredLength(), 25)
-        v.normalize()
-        self.assertEqual(v.length(), 1)
-        self.assertEqual(v.squaredLength(), 1)
-        self.assertEqual(v.x, 0.6)
-        self.assertEqual(v.y, 0.8)
+        for v in [Vec2d(3, 4), Vec2d(-3, 4), Vec2d(3, -4), Vec2d(-3, -4)]:
+            self.assertEqual(v.length(), 5)
+            self.assertEqual(v.squaredLength(), 25)
 
     def testNormalize(self):
         v = Vec2d(3, 4)
         v.normalize()
         self.assertEqual(v.length(), 1)
-        self.assertEqual(v.squaredLength(), 1)
-        self.assertEqual(v.x, 0.6)
-        self.assertEqual(v.y, 0.8)
+        self.assertEqual(v, Vec2d(0.6, 0.8))
 
     def testNormalized(self):
         v1 = Vec2d(3, 4)
         v2 = v1.normalized()
-        self.assertEqual(v1.length(), 5)
         self.assertEqual(v2.length(), 1)
-        self.assertEqual(v2.x, 0.6)
-        self.assertEqual(v2.y, 0.8)
+        self.assertEqual(v1, Vec2d(3, 4))
+        self.assertEqual(v2, Vec2d(0.6, 0.8))
 
     def testOrthogonalize(self):
         v = Vec2d(3, 4)
         v.orthogonalize()
-        self.assertEqual(v.x, -4)
-        self.assertEqual(v.y, 3)
+        self.assertEqual(v, Vec2d(-4, 3))
 
     def testOrthogonalized(self):
         v1 = Vec2d(3, 4)
         v2 = v1.orthogonalized()
-        self.assertEqual(v1.x, 3)
-        self.assertEqual(v1.y, 4)
-        self.assertEqual(v2.x, -4)
-        self.assertEqual(v2.y, 3)
+        self.assertEqual(v1, Vec2d(3, 4))
+        self.assertEqual(v2, Vec2d(-4, 3))
 
 if __name__ == '__main__':
     unittest.main()
