8a6acf2460044c26e1223df1c06a1fdf4dd8328b
Boris Dalstein <dalboris@gmail.com>
2018-08-01 14:03:02 +0200

    Wrap vgc::core::LogicError

--- libs/vgc/core/wraps/wrap_exceptions.cpp
+++ libs/vgc/core/wraps/wrap_exceptions.cpp
@@ -15,21 +15,9 @@
 // limitations under the License.
 
 #include <vgc/core/wraps/common.h>
+#include <vgc/core/exceptions.h>
 
-void wrap_color(py::module& m);
-void wrap_doublearray(py::module& m);
-void wrap_exceptions(py::module& m);
-void wrap_object(py::module& m);
-void wrap_timer(py::module& m);
-void wrap_vec2d(py::module& m);
-void wrap_vec2darray(py::module& m);
-
-PYBIND11_MODULE(core, m) {
-    wrap_color(m);
-    wrap_doublearray(m);
-    wrap_exceptions(m);
-    wrap_timer(m);
-    wrap_object(m);
-    wrap_vec2d(m);
-    wrap_vec2darray(m);
+void wrap_exceptions(py::module& m)
+{
+    py::register_exception<vgc::core::LogicError>(m, "LogicError");
 }
