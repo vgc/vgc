1cf49d4132711fb7e85a7e8d49cc22062f7e37f1
Boris Dalstein <dalboris@gmail.com>
2020-01-09 18:38:56 +0100

    Add dummy function to endure modoule is non-empty

---
+++ libs/vgc/ui/dummy.cpp
@@ -0,0 +1,11 @@
+namespace vgc {
+namespace ui {
+
+// Dummy function to ensure that this module is non-empty,
+// otherwise it causes linking errors with Visual Studio
+int dummy() {
+    return 0;
+}
+
+}
+}
