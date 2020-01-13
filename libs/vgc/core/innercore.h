ebbc3ee6b5c7ba9d9a7e3b9a34e61c9dcb77b9cd
Boris Dalstein <dalboris@gmail.com>
2020-01-13 11:18:28 +0100

    Create innercore.h header (#257)

---
+++ libs/vgc/core/innercore.h
@@ -0,0 +1,31 @@
+// Copyright 2020 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VGC_CORE_INNERCORE_H
+#define VGC_CORE_INNERCORE_H
+
+/// \file vgc/core/innercore.h
+/// \brief Includes the most fundamental parts of vgc::core.
+///
+/// This is a convenient header file which includes the most fundamental parts
+/// of vgc::core, which are typically needed by any compilation unit using the
+/// VGC C++ API, such as the VGC integer types and logging.
+///
+
+#include <vgc/core/int.h>
+#include <vgc/core/logging.h>
+
+#endif // VGC_CORE_INNERCORE_H
