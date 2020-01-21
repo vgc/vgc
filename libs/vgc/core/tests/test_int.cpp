55cd73f0e20d8a0ac553cb9291c04221f5058b95
Boris Dalstein <dalboris@gmail.com>
2020-01-21 14:11:39 +0100

    Update test_int.cpp

--- libs/vgc/core/tests/test_int.cpp
+++ libs/vgc/core/tests/test_int.cpp
@@ -6,34 +6,35 @@
 #include <vgc/core/int.h>
 
 using namespace vgc;
-using core::RangeError;
+using core::NegativeIntegerError;
+using core::IntegerOverflowError;
 
 int main()
 {
     // Trivial casts from type T to T.
     // TODO: also test with the minimum value and the max value.
-    int_cast<bool>(static_cast<bool>(0));
-    int_cast<char>(static_cast<char>(0));
-    int_cast<signed char>(static_cast<signed char>(0));
-    int_cast<unsigned char>(static_cast<unsigned char>(0));
-    int_cast<short>(static_cast<short>(0));
-    int_cast<int>(static_cast<int>(0));
-    int_cast<long>(static_cast<long>(0));
-    int_cast<long long>(static_cast<long long>(0));
-    int_cast<unsigned short>(static_cast<unsigned short>(0));
-    int_cast<unsigned int>(static_cast<unsigned int>(0));
-    int_cast<unsigned long>(static_cast<unsigned long>(0));
-    int_cast<unsigned long long>(static_cast<unsigned long long>(0));
-    int_cast<Int>(static_cast<Int>(0));
-    int_cast<Int8>(static_cast<Int8>(0));
-    int_cast<Int16>(static_cast<Int16>(0));
-    int_cast<Int32>(static_cast<Int32>(0));
-    int_cast<Int64>(static_cast<Int64>(0));
-    int_cast<UInt>(static_cast<UInt>(0));
-    int_cast<UInt8>(static_cast<UInt8>(0));
-    int_cast<UInt16>(static_cast<UInt16>(0));
-    int_cast<UInt32>(static_cast<UInt32>(0));
-    int_cast<UInt64>(static_cast<UInt64>(0));
+    int_cast<bool>(bool(0));
+    int_cast<char>(char(0));
+    int_cast<signed char>((signed char)(0));
+    int_cast<unsigned char>((unsigned char)(0));
+    int_cast<short>(short(0));
+    int_cast<int>(int(0));
+    int_cast<long>(long(0));
+    int_cast<long long>((long long)(0));
+    int_cast<unsigned short>((unsigned short)(0));
+    int_cast<unsigned int>((unsigned int)(0));
+    int_cast<unsigned long>((unsigned long)(0));
+    int_cast<unsigned long long>((unsigned long long)(0));
+    int_cast<Int>(Int(0));
+    int_cast<Int8>(Int8(0));
+    int_cast<Int16>(Int16(0));
+    int_cast<Int32>(Int32(0));
+    int_cast<Int64>(Int64(0));
+    int_cast<UInt>(UInt(0));
+    int_cast<UInt8>(UInt8(0));
+    int_cast<UInt16>(UInt16(0));
+    int_cast<UInt32>(UInt32(0));
+    int_cast<UInt64>(UInt64(0));
 
     // Casts between separate types that should work.
     // TODO: be more exhaustive than that.
@@ -47,13 +48,13 @@ int main()
 
     // Casts between separate types that should raise an exception.
     // TODO: be more exhaustive than that.
-    try { int_cast<UInt8>(Int8(-1)); abort(); } catch (RangeError&) {}
-    try { int_cast<UInt8>(Int16(256)); abort(); } catch (RangeError&) {}
-    try { int_cast<UInt32>(Int32(-1)); abort(); } catch (RangeError&) {}
-    try { int_cast<UInt32>(Int64(4294967296)); abort(); } catch (RangeError&) {}
-    try { int_cast<Int8>(UInt8(128)); abort(); } catch (RangeError&) {}
-    try { int_cast<Int8>(Int16(128)); abort(); } catch (RangeError&) {}
-    try { int_cast<UInt8>(UInt16(256)); abort(); } catch (RangeError&) {}
+    try { int_cast<UInt8>(Int8(-1)); abort(); } catch (NegativeIntegerError&) {}
+    try { int_cast<UInt8>(Int16(256)); abort(); } catch (IntegerOverflowError&) {}
+    try { int_cast<UInt32>(Int32(-1)); abort(); } catch (NegativeIntegerError&) {}
+    try { int_cast<UInt32>(Int64(4294967296)); abort(); } catch (IntegerOverflowError&) {}
+    try { int_cast<Int8>(UInt8(128)); abort(); } catch (IntegerOverflowError&) {}
+    try { int_cast<Int8>(Int16(128)); abort(); } catch (IntegerOverflowError&) {}
+    try { int_cast<UInt8>(UInt16(256)); abort(); } catch (IntegerOverflowError&) {}
 
     return 0;
 }
