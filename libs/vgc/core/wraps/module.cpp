c19f7409ff959bd2f77abbe28624f02930334fb1
Boris Dalstein <dalboris@gmail.com>
2020-02-18 22:47:53 +0100

    Define generic helper function to wrap all 1D arrays (#38)

--- libs/vgc/core/wraps/module.cpp
+++ libs/vgc/core/wraps/module.cpp
@@ -16,8 +16,8 @@
 
 #include <vgc/core/wraps/common.h>
 
+void wrap_arrays(py::module& m);
 void wrap_color(py::module& m);
-void wrap_doublearray(py::module& m);
 void wrap_exceptions(py::module& m);
 void wrap_format(py::module& m);
 void wrap_io(py::module& m);
@@ -29,8 +29,8 @@ void wrap_vec2d(py::module& m);
 void wrap_vec2darray(py::module& m);
 
 PYBIND11_MODULE(core, m) {
+    wrap_arrays(m);
     wrap_color(m);
-    wrap_doublearray(m);
     wrap_exceptions(m);
     wrap_format(m);
     wrap_io(m);
