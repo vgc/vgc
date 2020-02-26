5a1b41e97f864a9031285919c38fff49e7281e68
Boris Dalstein <dalboris@gmail.com>
2020-02-26 20:23:01 +0100

    Color palette: Fix blurriness (round to exact pixels); Add borders; Polish (#270)

--- libs/vgc/ui/colorpalette.cpp
+++ libs/vgc/ui/colorpalette.cpp
@@ -33,7 +33,7 @@ ColorPalette::ColorPalette(const ConstructorKey&) :
     reload_(true),
     margin_(15),
     numHueSteps_(12),
-    numSaturationSteps_(7),
+    numSaturationSteps_(5),
     numLightnessSteps_(7),
     hoveredHueIndex_(-1),
     hoveredSaturationIndex_(-1),
@@ -61,20 +61,34 @@ void ColorPalette::onPaintDraw(graphics::Engine* engine)
         oldWidth_ = width();
         oldHeight_ = height();
         core::FloatArray a;
+
+        // Draw saturation/lightness selector
         float x0 = margin_;
         float y0 = margin_;
         float w = width() - 2*margin_;
-        float dx = w / numLightnessSteps_;
-        float dy = dx;
-        float h = dy * numSaturationSteps_;
-        double hue = 330;
+        float dx = (w - 1) / numLightnessSteps_;
+        float dy = std::round(dx);
+        float h = 1 + dy * numSaturationSteps_;
+        float x, y, r, g, b;
+        x = x0; y = y0;
+        r = 0.0; g = 0.0; b = 0.0;
+        a.insert(a.end(), {
+            x,   y,   r, g, b,
+            x+w, y,   r, g, b,
+            x,   y+h, r, g, b,
+            x+w, y,   r, g, b,
+            x+w, y+h, r, g, b,
+            x,   y+h, r, g, b});
+        double hue = 210;
         double dl = 1.0 / (numLightnessSteps_ - 1);
         double ds = 1.0 / (numSaturationSteps_ - 1);
         for (Int i = 0; i < numLightnessSteps_; ++i) {
-            float x = x0 + i*dx;
+            float x1 = std::round(x0 + 1 + i*dx);
+            float x2 = std::round(x0 + (i+1)*dx);
             double l = i*dl;
             for (Int j = 0; j < numSaturationSteps_; ++j) {
-                float y = y0 + j*dy;
+                float y1 = y0 + 1 + j*dy;
+                float y2 = y1 + dy - 1;
                 double s = j*ds;
                 auto c = core::Color::hsl(hue, s, l);
                 float r = static_cast<float>(c[0]);
@@ -82,71 +96,94 @@ void ColorPalette::onPaintDraw(graphics::Engine* engine)
                 float b = static_cast<float>(c[2]);
                 // TODO: implement a.extend()
                 a.insert(a.end(), {
-                    x,    y,    r, g, b,
-                    x+dx, y,    r, g, b,
-                    x,    y+dy, r, g, b,
-                    x+dx, y,    r, g, b,
-                    x+dx, y+dy, r, g, b,
-                    x,    y+dy, r, g, b});
+                    x1, y1, r, g, b,
+                    x2, y1, r, g, b,
+                    x1, y2, r, g, b,
+                    x2, y1, r, g, b,
+                    x2, y2, r, g, b,
+                    x1, y2, r, g, b});
             }
         }
         if (hoveredLightnessIndex_ != -1) {
-            float o = 2; // pixel offset
-            float ro = 0.0f;
-            float go = 0.0f;
-            float bo = 0.0f;
-            float x = x0 + hoveredLightnessIndex_*dx;
-            float y = x0 + hoveredSaturationIndex_*dy;
-            double l = hoveredLightnessIndex_*dl;
-            double s = hoveredSaturationIndex_*ds;
+            Int i = hoveredLightnessIndex_;
+            Int j = hoveredSaturationIndex_;
+            float x1 = std::round(x0 + 1 + i*dx);
+            float x2 = std::round(x0 + (i+1)*dx);
+            float y1 = y0 + 1 + j*dy;
+            float y2 = y1 + dy - 1;
+            double l = i*dl;
+            double s = j*ds;
             auto c = core::Color::hsl(hue, s, l);
             float r = static_cast<float>(c[0]);
             float g = static_cast<float>(c[1]);
             float b = static_cast<float>(c[2]);
+            float x1h = x1 - 3;
+            float x2h = x2 + 3;
+            float y1h = y1 - 3;
+            float y2h = y2 + 3;
+            float rh = 0.043; //
+            float gh = 0.322; // VGC Blue
+            float bh = 0.714; //
             a.insert(a.end(), {
-                 x-o,    y-o,    ro, go, bo,
-                 x+dx+o, y-o,    ro, go, bo,
-                 x-o,    y+dy+o, ro, go, bo,
-                 x+dx+o, y-o,    ro, go, bo,
-                 x+dx+o, y+dy+o, ro, go, bo,
-                 x-o,    y+dy+o, ro, go, bo,
-                 x,    y,    r, g, b,
-                 x+dx, y,    r, g, b,
-                 x,    y+dy, r, g, b,
-                 x+dx, y,    r, g, b,
-                 x+dx, y+dy, r, g, b,
-                 x,    y+dy, r, g, b});
+                x1h, y1h, rh, gh, bh,
+                x2h, y1h, rh, gh, bh,
+                x1h, y2h, rh, gh, bh,
+                x2h, y1h, rh, gh, bh,
+                x2h, y2h, rh, gh, bh,
+                x1h, y2h, rh, gh, bh,
+                x1, y1, r, g, b,
+                x2, y1, r, g, b,
+                x1, y2, r, g, b,
+                x2, y1, r, g, b,
+                x2, y2, r, g, b,
+                x1, y2, r, g, b});
         }
-        //float hueHeight = 0.2 * w;
-        double dhue = 360.0 / numHueSteps_;
+
+        // Draw hue selector
+        y0 += h + margin_;
         Int halfNumHueSteps = numHueSteps_ / 2;
-        float y1 = y0 + h + margin_;
-        dx = w / halfNumHueSteps;
-        dy = dx;
-        float y2 = y1 + dy;
+        dx = (w - 1) / halfNumHueSteps;
+        dy = std::round(dx);
+        h = 1 + dy * 2;
+        x = x0; y = y0;
+        r = 0.0; g = 0.0; b = 0.0;
+        a.insert(a.end(), {
+            x,   y,   r, g, b,
+            x+w, y,   r, g, b,
+            x,   y+h, r, g, b,
+            x+w, y,   r, g, b,
+            x+w, y+h, r, g, b,
+            x,   y+h, r, g, b});
+        double dhue = 360.0 / numHueSteps_;
+        float y01 = y0 + 1;
+        float y02 = y01 + dy;
+        float y03 = y02 + dy;
         for (Int i = 0; i < numHueSteps_; ++i) {
-            float x, y;
+            float x1, y1, x2, y2;
             if (i < halfNumHueSteps) {
-                x = x0 + i*dx;
-                y = y1;
+                x1 = std::round(x0 + 1 + i*dx);
+                x2 = std::round(x0 + (i+1)*dx);
+                y1 = y01;
+                y2 = y02 - 1;
             }
             else {
-                x = x0 + (numHueSteps_ - i - 1)*dx;
-                y = y2;
+                x1 = std::round(x0 + 1 + (numHueSteps_ - i - 1)*dx);
+                x2 = std::round(x0 + (numHueSteps_ - i)*dx);
+                y1 = y02;
+                y2 = y03 - 1;
             }
             double hue = i*dhue;
             auto c = core::Color::hsl(hue, 1.0, 0.5);
             float r = static_cast<float>(c[0]);
             float g = static_cast<float>(c[1]);
             float b = static_cast<float>(c[2]);
-            // TODO: implement a.extend()
             a.insert(a.end(), {
-                x,    y,    r, g, b,
-                x+dx, y,    r, g, b,
-                x,    y+dy, r, g, b,
-                x+dx, y,    r, g, b,
-                x+dx, y+dy, r, g, b,
-                x,    y+dy, r, g, b});
+                x1, y1, r, g, b,
+                x2, y1, r, g, b,
+                x1, y2, r, g, b,
+                x2, y1, r, g, b,
+                x2, y2, r, g, b,
+                x1, y2, r, g, b});
         }
         engine->loadTriangles(trianglesId_, a.data(), a.length());
     }
@@ -166,7 +203,9 @@ bool ColorPalette::onMouseMove(MouseEvent* event)
     float x0 = margin_;
     float y0 = margin_;
     float w = width() - 2*margin_;
-    float h = w;
+    float dx = (w - 1) / numLightnessSteps_;
+    float dy = std::round(dx);
+    float h = 1 + dy * numSaturationSteps_;
     const core::Vec2f& p = event->pos();
     if (p[0] > x0 && p[0] < x0+w && p[1] > y0 && p[1] < y0+h) {
         float i_ = numLightnessSteps_ * (p[0]-x0) / w;
