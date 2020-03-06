ddffa4a9cf157a8dd2e94d337476876faf4e34f0
Boris Dalstein <dalboris@gmail.com>
2020-03-06 01:46:59 +0100

    Start graphics::FontLibrary (#369) (#373)

---
+++ libs/vgc/graphics/exceptions.h
@@ -0,0 +1,43 @@
+// Copyright 2020 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VGC_GRAPHICS_EXCEPTIONS_H
+#define VGC_GRAPHICS_EXCEPTIONS_H
+
+#include <vgc/core/exceptions.h>
+#include <vgc/graphics/api.h>
+
+namespace vgc {
+namespace graphics {
+
+/// \class vgc::graphics::FontError
+/// \brief Raised when there is an error related to fonts.
+///
+class VGC_GRAPHICS_API FontError : public core::RuntimeError {
+public:
+    /// Constructs a FontError with the given \p reason.
+    ///
+    explicit FontError(const std::string& reason) : core::RuntimeError(reason) {}
+
+    /// Destructs the FontError.
+    ///
+    ~FontError();
+};
+
+} // namespace graphics
+} // namespace vgc
+
+#endif // VGC_GRAPHICS_EXCEPTIONS_H
