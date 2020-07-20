1e5bc5cc9f84f22b8c393eb74733d0f10906bdcf
Boris Dalstein <dalboris@gmail.com>
2020-07-20 17:57:33 +0200

    Start FontGlyph; Get glyph from index or Unicode code point (#369) (#390)

--- libs/vgc/graphics/font.h
+++ libs/vgc/graphics/font.h
@@ -27,6 +27,7 @@ namespace internal {
 
 class FontLibraryImpl;
 class FontFaceImpl;
+class FontGlyphImpl;
 
 // See: https://stackoverflow.com/questions/9954518/stdunique-ptr-with-an-incomplete-type-wont-compile
 // TODO: vgc/core/pimpl.h for helper macros.
@@ -36,13 +37,18 @@ struct FontLibraryImplDeleter {
 struct FontFaceImplDeleter {
     void operator()(FontFaceImpl* p);
 };
-using FontFacePimpl = std::unique_ptr<FontFaceImpl, FontFaceImplDeleter>;
+struct FontGlyphImplDeleter {
+    void operator()(FontGlyphImpl* p);
+};
 using FontLibraryPimpl = std::unique_ptr<FontLibraryImpl, FontLibraryImplDeleter>;
+using FontFacePimpl = std::unique_ptr<FontFaceImpl, FontFaceImplDeleter>;
+using FontGlyphPimpl = std::unique_ptr<FontGlyphImpl, FontGlyphImplDeleter>;
 
 } // namespace internal
 
 VGC_DECLARE_OBJECT(FontLibrary);
 VGC_DECLARE_OBJECT(FontFace);
+VGC_DECLARE_OBJECT(FontGlyph);
 
 /// \class vgc::graphics::FontLibrary
 /// \brief Manages a set of available fonts.
@@ -107,10 +113,41 @@ protected:
     /// detail. In order to create a FontFace, please use the following:
     ///
     /// ```cpp
-    /// FontFace* fontFace = fontLibrary->addFace();
+    /// FontFace* fontFace = fontLibrary->addFace(filename);
     /// ```
     ///
     FontFace(FontLibrary* library);
+
+public:
+    /// Returns the glyph corresponding to the given Unicode code point, or
+    /// nullptr if this face doesn't have a glyph for this code point.
+    ///
+    /// ```cpp
+    /// FontGlyph* fontGlyph = fontFace->getGlyphFromCodePoint(0x0041); // => 'A'
+    /// ```
+    ///
+    /// This function is equivalent to calling getGlyphIndexFromCodePoint() then
+    /// getGlyphFromIndex().
+    ///
+    /// Note that some glyphs may not be accessible via this function, because
+    /// they do not correspond to any code point. If you need to access such
+    /// glyphs, you must instead use getGlyphFromIndex() directly.
+    ///
+    FontGlyph* getGlyphFromCodePoint(UInt32 codePoint);
+
+    /// Returns the glyph at the given glyph index. This uses an internal
+    /// indexing system, which may or may not be equal to the indices used in
+    /// the font file, or to Unicode code points.
+    ///
+    /// Raises a vgc::core::FontError if the given glyphIndex is not a valid
+    /// index or another error occurs.
+    ///
+    FontGlyph* getGlyphFromIndex(UInt32 glyphIndex);
+
+    /// Returns the glyph index corresponding to the given Unicode code point,
+    /// or 0 if this face doesn't have a glyph for this code point.
+    ///
+    UInt32 getGlyphIndexFromCodePoint(UInt32 codePoint);
 
 protected:
     /// \reimp
@@ -120,6 +157,48 @@ private:
     internal::FontFacePimpl impl_;
     friend class FontLibrary;
     friend class internal::FontLibraryImpl;
+};
+
+/// \class vgc::graphics::FontGlyph
+/// \brief A given glyph of a given FontFace.
+///
+class VGC_GRAPHICS_API FontGlyph : public core::Object {
+private:
+    VGC_OBJECT(FontGlyph)
+    VGC_PRIVATIZE_OBJECT_TREE_MUTATORS
+
+protected:
+    /// Creates a new FontGlyph. This constructor is an implementation detail.
+    /// In order to get a glyph in a given face, please use one of the following:
+    ///
+    /// ```cpp
+    /// FontGlyph* fontGlyph1 = fontFace->getGlyphFromCodePoint(codePoint);
+    /// FontGlyph* fontGlyph2 = fontFace->getGlyphFromIndex(glyphIndex);
+    /// ```
+    ///
+    FontGlyph(FontFace* face);
+
+public:
+    /// Returns the index of this glyph. This is an integer that can be
+    /// used to retrieve the glyph via `face->getGlyphFromIndex()`.
+    ///
+    UInt32 index() const;
+
+    /// Returns the name of this glyph, or an empty string if the face doesn't
+    /// support glyph names.
+    ///
+    std::string name() const;
+
+protected:
+    /// \reimp
+    void onDestroyed() override;
+
+private:
+    internal::FontGlyphPimpl impl_;
+    friend class FontLibrary;
+    friend class FontFace;
+    friend class internal::FontLibraryImpl;
+    friend class internal::FontFaceImpl;
 };
 
 } // namespace graphics
