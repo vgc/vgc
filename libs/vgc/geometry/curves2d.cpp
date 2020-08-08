d3f68537e9cab3074878c1ed4bb9db69c8ef1f15
Boris Dalstein <dalboris@gmail.com>
2020-08-08 20:19:41 +0200

    Implement convenient moveTo/lineTo/etc methods and proxy iterators for Curves2d (#392) (#403)

--- libs/vgc/geometry/curves2d.cpp
+++ libs/vgc/geometry/curves2d.cpp
@@ -19,13 +19,71 @@
 namespace vgc {
 namespace geometry {
 
-void Curves2d::addCommand(
-        CurveCommandType type, bool isRelative,
-        const core::DoubleArray& args)
+void Curves2d::close()
 {
-    commands_.append(CurveCommand(type, isRelative, args.length()));
-    arguments_.insert(arguments_.end(), args.begin(), args.end());
-    // TODO: implement Array::extend() and use arguments_.extend(args).
+    commandData_.append({CurveCommandType::Close, data_.length()});
+}
+
+void Curves2d::moveTo(const core::Vec2d& p)
+{
+    moveTo(p[0], p[1]);
+}
+
+void Curves2d::moveTo(double x, double y)
+{
+    data_.append(x);
+    data_.append(y);
+    commandData_.append({CurveCommandType::MoveTo, data_.length()});
+}
+
+void Curves2d::lineTo(const core::Vec2d& p)
+{
+    lineTo(p[0], p[1]);
+}
+
+void Curves2d::lineTo(double x, double y)
+{
+    // TODO (for all functions): support subcommands, that is, don't add a new
+    // command if the last command is the same. We should simply add more params.
+    data_.append(x);
+    data_.append(y);
+    commandData_.append({CurveCommandType::LineTo, data_.length()});
+}
+
+void Curves2d::quadraticBezierTo(const core::Vec2d& p1,
+                                 const core::Vec2d& p2)
+{
+    quadraticBezierTo(p1[0], p1[1], p2[0], p2[1]);
+}
+
+void Curves2d::quadraticBezierTo(double x1, double y1,
+                                 double x2, double y2)
+{
+    data_.append(x1);
+    data_.append(y1);
+    data_.append(x2);
+    data_.append(y2);
+    commandData_.append({CurveCommandType::QuadraticBezierTo, data_.length()});
+}
+
+void Curves2d::cubicBezierTo(const core::Vec2d& p1,
+                             const core::Vec2d& p2,
+                             const core::Vec2d& p3)
+{
+    cubicBezierTo(p1[0], p1[1], p2[0], p2[1], p3[0], p3[1]);
+}
+
+void Curves2d::cubicBezierTo(double x1, double y1,
+                             double x2, double y2,
+                             double x3, double y3)
+{
+    data_.append(x1);
+    data_.append(y1);
+    data_.append(x2);
+    data_.append(y2);
+    data_.append(x3);
+    data_.append(y3);
+    commandData_.append({CurveCommandType::CubicBezierTo, data_.length()});
 }
 
 } // namespace geometry
