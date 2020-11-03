5c2f9157947a5fc8b69b16c52fe7700e77b182d1
Boris Dalstein <dalboris@gmail.com>
2020-11-03 19:12:02 +0100

    Start implementation of ui::Button (#448)

---
+++ libs/vgc/ui/button.h
@@ -0,0 +1,64 @@
+// Copyright 2020 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VGC_UI_BUTTON_H
+#define VGC_UI_BUTTON_H
+
+#include <vgc/ui/widget.h>
+
+namespace vgc {
+namespace ui {
+
+VGC_DECLARE_OBJECT(Button);
+
+/// \class vgc::ui::Button
+/// \brief A button widget.
+///
+class VGC_UI_API Button : public Widget {
+private:
+    VGC_OBJECT(Button)
+
+protected:
+    /// This is an implementation details. Please use
+    /// Button::create() instead.
+    ///
+    Button();
+
+public:
+    /// Creates a Button.
+    ///
+    static ButtonPtr create();
+
+    // reimpl
+    void onPaintCreate(graphics::Engine* engine) override;
+    void onPaintDraw(graphics::Engine* engine) override;
+    void onPaintDestroy(graphics::Engine* engine) override;
+    bool onMouseMove(MouseEvent* event) override;
+    bool onMousePress(MouseEvent* event) override;
+    bool onMouseRelease(MouseEvent* event) override;
+    bool onMouseEnter() override;
+    bool onMouseLeave() override;
+
+private:
+    Int trianglesId_;
+    bool reload_;
+    bool isHovered_;
+};
+
+} // namespace ui
+} // namespace vgc
+
+#endif // VGC_UI_BUTTON_H
