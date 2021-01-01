7f03896bd0a191edfe26638010e9161f46620359
Boris Dalstein <dalboris@gmail.com>
2021-01-01 09:50:47 +0100

    Bump copyright year to 2021 (#241) (#483)

--- libs/vgc/ui/tests/test_widget.py
+++ libs/vgc/ui/tests/test_widget.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2020 The VGC Developers
+# Copyright 2021 The VGC Developers
 # See the COPYRIGHT file at the top-level directory of this distribution
 # and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
 #
