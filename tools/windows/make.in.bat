7d93b41f487502822774af756f6a67586797e89c
Boris Dalstein <dalboris@gmail.com>
2022-02-03 23:20:04 +0100

    Don't add cmake --target argument in make.bat if there is no argument

--- tools/windows/make.in.bat
+++ tools/windows/make.in.bat
@@ -6,5 +6,13 @@ setlocal
 :: Setup Visual Studio development environment
 call "${CMAKE_GENERATOR_INSTANCE}\Common7\Tools\VsDevCmd.bat" -arch=amd64
 
+if "%~1"=="" (target="") else (target="--target %1")
+
+echo %*
+echo %0
+echo %1
+echo %2
+echo %target%
+
 :: Build the given target with the default config
-cmake --build . --target %* --config ${DefaultConfig}
+cmake --build . %target% --config ${DefaultConfig}
