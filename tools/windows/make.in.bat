f3a4c1972e6b1de6ec18788a0fb4a827156bfc83
Boris Dalstein <dalboris@gmail.com>
2022-02-03 23:48:41 +0100

    Fix batch script

--- tools/windows/make.in.bat
+++ tools/windows/make.in.bat
@@ -1,18 +1,15 @@
 :: Don't print commands before executing them and keep variables
 :: and working directory local to this script
-@echo on
+@echo off
 setlocal
 
 :: Setup Visual Studio development environment
 call "${CMAKE_GENERATOR_INSTANCE}\Common7\Tools\VsDevCmd.bat" -arch=amd64
 
-if "%~1"=="" (target="") else (target="--target %1")
-
-echo %*
-echo %0
-echo %1
-echo %2
-echo %target%
-
 :: Build the given target with the default config
-cmake --build . %target% --config ${DefaultConfig}
+if "%~1"=="" (
+    set TARGET=""
+) else (
+    set TARGET = "--target %1"
+)
+cmake --build . %TARGET% --config ${DefaultConfig}
