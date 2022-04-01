eba208ca1f5582b085b256504defd83c168fb0e3
Boris Dalstein <dalboris@gmail.com>
2022-04-01 12:54:59 +0200

    Improve target dependency graph, IDE target view, and add per-lib check target (#90)

--- tools/VgcTools.cmake
+++ tools/VgcTools.cmake
@@ -13,6 +13,21 @@ function(vgc_prepend_ var prefix)
    set(${var} "${listVar}" PARENT_SCOPE)
 endfunction()
 
+# Prefixes and suffixes all strings of a comma-separated list of string,
+# and stores the result in a new variable.
+#
+# Usage:
+# vgc_add_prefix_suffix_(s "vgc_" "core;ui" "_lib")
+# message(${s}) # -> "vgc_core_lib;vgc_ui_lib"
+#
+function(vgc_add_prefix_suffix_ var prefix middle suffix)
+   set(listVar "")
+   foreach(f ${middle})
+      list(APPEND listVar "${prefix}${f}${suffix}")
+   endforeach()
+   set(${var} "${listVar}" PARENT_SCOPE)
+endfunction()
+
 # Defines a new VGC library. This calls add_library under the hood.
 #
 # Usage:
@@ -39,7 +54,10 @@ endfunction()
 # )
 #
 function(vgc_add_library LIB_NAME)
-    message(STATUS "vgc::${LIB_NAME}")
+    set(BASE_TARGET      vgc_${LIB_NAME})
+    set(LIB_TARGET       vgc_${LIB_NAME}_lib)
+    set(RESOURCES_TARGET vgc_${LIB_NAME}_resources)
+    message(STATUS ${LIB_TARGET})
 
     set(options "")
     set(oneValueArgs "")
@@ -52,9 +70,8 @@ function(vgc_add_library LIB_NAME)
             RESOURCE_FILES)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Add library using target name "vgc_lib_mylib"
-    set(TARGET_NAME vgc_lib_${LIB_NAME})
-    add_library(${TARGET_NAME} SHARED ${ARG_HEADER_FILES} ${ARG_CPP_FILES})
+    # Create library target "vgc_<libname>_lib"
+    add_library(${LIB_TARGET} SHARED ${ARG_HEADER_FILES} ${ARG_CPP_FILES})
 
     # Set library output directory and filenames.
     #
@@ -86,7 +103,7 @@ function(vgc_add_library LIB_NAME)
     #   Output on Linux with Make generator: <build>/lib/libvgccore.so
     #   Output on Windows with VS generator: <build>/<Config>/bin/vgccore.dll
     #
-    set_target_properties(${TARGET_NAME}
+    set_target_properties(${LIB_TARGET}
         PROPERTIES
             OUTPUT_NAME vgc${LIB_NAME}
             LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/lib
@@ -95,60 +112,78 @@ function(vgc_add_library LIB_NAME)
     )
 
     # Add dependencies to other VGC libraries
-    vgc_prepend_(VGC_DEPENDENCIES vgc_lib_ ${ARG_VGC_DEPENDENCIES})
-    target_link_libraries(${TARGET_NAME} PUBLIC ${VGC_DEPENDENCIES})
+    vgc_add_prefix_suffix_(VGC_LIB_DEPENDENCIES vgc_ "${ARG_VGC_DEPENDENCIES}" _lib)
+    target_link_libraries(${LIB_TARGET} PUBLIC ${VGC_LIB_DEPENDENCIES})
 
     # Add dependencies to third-party dependencies
-    target_link_libraries(${TARGET_NAME} PUBLIC ${ARG_THIRD_DEPENDENCIES})
+    target_link_libraries(${LIB_TARGET} PUBLIC ${ARG_THIRD_DEPENDENCIES})
 
     # Set compile definitions, that is, values given to preprocessor variables
     # These are public, that is, they propagate to dependent libraries.
-    target_compile_definitions(${TARGET_NAME} PUBLIC ${ARG_COMPILE_DEFINITIONS})
+    target_compile_definitions(${LIB_TARGET} PUBLIC ${ARG_COMPILE_DEFINITIONS})
 
     # Set -DVGC_FOO_EXPORTS when compiling this library (but not when using it)
     # to properly export DLL symbols. See libs/vgc/core/dll.h for details.
     string(TOUPPER VGC_${LIB_NAME}_EXPORTS EXPORTS_COMPILE_DEFINITION)
-    target_compile_definitions(${TARGET_NAME} PRIVATE ${EXPORTS_COMPILE_DEFINITION})
+    target_compile_definitions(${LIB_TARGET} PRIVATE ${EXPORTS_COMPILE_DEFINITION})
 
     # Add -fvisibility=hidden when compiling on Linux/macOS.
     # Note: This is already the default on Windows.
-    set_target_properties(${TARGET_NAME} PROPERTIES CXX_VISIBILITY_PRESET hidden)
+    set_target_properties(${LIB_TARGET} PROPERTIES CXX_VISIBILITY_PRESET hidden)
 
     # Add compiler warning flags and macros
-    target_compile_options(${TARGET_NAME} PUBLIC ${VGC_COMPILER_WARNING_FLAGS})
-    target_compile_definitions(${TARGET_NAME} PUBLIC ${VGC_COMPILER_WARNING_DEFINITIONS})
+    target_compile_options(${LIB_TARGET} PUBLIC ${VGC_COMPILER_WARNING_FLAGS})
+    target_compile_definitions(${LIB_TARGET} PUBLIC ${VGC_COMPILER_WARNING_DEFINITIONS})
 
     # Add private compile definitions
-    target_compile_definitions(${TARGET_NAME} PUBLIC ${VGC_PRIVATE_COMPILE_DEFINITIONS})
+    target_compile_definitions(${LIB_TARGET} PUBLIC ${VGC_PRIVATE_COMPILE_DEFINITIONS})
+    
+    # Copy resources to the build folder whenever necessary
+    if(ARG_RESOURCE_FILES)
+        
+        # Write list of resources as comma-separated string.
+        # We use this in copy_resources.py to know which resources require to be updated.
+        set(RESOURCES_TXT ${CMAKE_CURRENT_BINARY_DIR}/resources.txt)
+        vgc_prepend_(RESOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/ ${ARG_RESOURCE_FILES})
+        add_custom_command(
+            COMMENT ""
+            OUTPUT ${RESOURCES_TXT}
+            DEPENDS ${RESOURCE_FILES}
+            COMMAND ${CMAKE_COMMAND} -E echo "${ARG_RESOURCE_FILES}" > ${RESOURCES_TXT}
+            VERBATIM
+        )
+                
+        # Create target executing our copy_resources.py script.
+        add_custom_target(${RESOURCES_TARGET}
+            VERBATIM
+            SOURCES ${ARG_RESOURCE_FILES}
+            DEPENDS ${RESOURCES_TXT}
+            COMMAND ${Python_EXECUTABLE}
+                "${CMAKE_SOURCE_DIR}/tools/copy_resources.py"
+                "${CMAKE_SOURCE_DIR}"
+                "${CMAKE_BINARY_DIR}"
+                "$<CONFIG>"
+                "lib"
+                "${LIB_NAME}"
+        )
+        set_target_properties(${RESOURCES_TARGET} PROPERTIES FOLDER libs/${LIB_NAME})
+        add_dependencies(${LIB_TARGET} ${RESOURCES_TARGET})
+    endif()
 
-    # Write list of resources as comma-separated string
-    set(RESOURCES_TXT ${CMAKE_CURRENT_BINARY_DIR}/resources.txt)
-    vgc_prepend_(RESOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/ ${ARG_RESOURCE_FILES})
-    add_custom_command(
-        COMMENT ""
-        OUTPUT ${RESOURCES_TXT}
-        DEPENDS ${RESOURCE_FILES}
-        COMMAND ${CMAKE_COMMAND} -E echo "${ARG_RESOURCE_FILES}" > ${RESOURCES_TXT}
-        VERBATIM
-    )
-
-    # Update build copy of resources whenever necessary
-    add_custom_target(${TARGET_NAME}_resources ALL
-        VERBATIM
-        DEPENDS ${RESOURCES_TXT}
-        COMMAND ${Python_EXECUTABLE}
-            "${CMAKE_SOURCE_DIR}/tools/copy_resources.py"
-            "${CMAKE_SOURCE_DIR}"
-            "${CMAKE_BINARY_DIR}"
-            "$<CONFIG>"
-            "lib"
-            "${LIB_NAME}"
-    )
-    set_target_properties(${TARGET_NAME}_resources
+    # Create vgc_<libname> base target, which is useful for:
+    # - building both the lib and its Python wrappers (vgc_<appname>_app needs this)
+    # - storing internal dependencies (vgc_<libname>_wraps needs this)
+    add_custom_target(${BASE_TARGET})
+    set_target_properties(${BASE_TARGET}
         PROPERTIES
-            FOLDER libs/${LIB_NAME}
+            FOLDER misc/libs
+            VGC_DEPENDENCIES "${ARG_VGC_DEPENDENCIES}"
     )
-    add_dependencies(${TARGET_NAME} ${TARGET_NAME}_resources)
+    add_dependencies(${BASE_TARGET} ${LIB_TARGET})
+    vgc_prepend_(BASE_TARGET_DEPENDENCIES vgc_ ${ARG_VGC_DEPENDENCIES})
+    if(BASE_TARGET_DEPENDENCIES)
+        add_dependencies(${BASE_TARGET} ${BASE_TARGET_DEPENDENCIES})
+    endif()
 
 endfunction()
 
@@ -175,34 +210,43 @@ endfunction()
 # )
 #
 function(vgc_wrap_library LIB_NAME)
-    message(STATUS "vgc::${LIB_NAME} wraps")
+    set(BASE_TARGET   vgc_${LIB_NAME})
+    set(LIB_TARGET    vgc_${LIB_NAME}_lib)
+    set(WRAPS_TARGET  vgc_${LIB_NAME}_wraps)
+    message(STATUS ${WRAPS_TARGET})
 
-    cmake_parse_arguments(ARG "" "" "" ${ARGN})
-
-    # Get cmake target name for the python module and the C++ lib
-    # Example:
-    #   LIB_NAME = geometry
-    #   LIB_TARGET_NAME = vgc_lib_geometry
-    #   WRAP_TARGET_NAME = vgc_wrap_geometry
-    vgc_prepend_(LIB_TARGET_NAME vgc_lib_ ${LIB_NAME})
-    vgc_prepend_(WRAP_TARGET_NAME vgc_wrap_ ${LIB_NAME})
-
+    cmake_parse_arguments(ARG "" "" "" ${ARGN})    
+    
     # Use pybind11 helper function. This calls add_library(${TARGET_NAME} ...)
     # and sets all required include dirs and libs to link.
-    pybind11_add_module(${WRAP_TARGET_NAME} ${ARG_UNPARSED_ARGUMENTS})
+    pybind11_add_module(${WRAPS_TARGET} ${ARG_UNPARSED_ARGUMENTS})
 
     # Set the output name. This name must match how python users
     # import the library, e.g., "from vgc.geometry import Point"
-    set_target_properties(${WRAP_TARGET_NAME}
+    set_target_properties(${WRAPS_TARGET}
         PROPERTIES
             OUTPUT_NAME ${LIB_NAME}
             LIBRARY_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/python/vgc
             RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/python/vgc
             FOLDER libs/${LIB_NAME}
     )
-
+    
     # Link to the C++ library this Python module is wrapping
-    target_link_libraries(${WRAP_TARGET_NAME} PRIVATE ${LIB_TARGET_NAME})
+    target_link_libraries(${WRAPS_TARGET} PRIVATE ${LIB_TARGET})
+    
+    # Add vgc_<libname>_wraps target as dependency to vgc_<libname> target
+    add_dependencies(${BASE_TARGET} ${WRAPS_TARGET})
+
+    # Add dependencies to the wrappers that this wrapper depends to. For
+    # example, when we build vgc_dom_wraps, we also need vgc_core_wraps to be
+    # built. We do this indirectly by adding a dependency to vgc_core which
+    # depends itself on vgc_wraps, which nicely handles the case where a lib
+    # doesn't have wrappers.
+    get_target_property(VGC_DEPENDENCIES ${BASE_TARGET} VGC_DEPENDENCIES)
+    vgc_prepend_(WRAPS_TARGET_DEPENDENCIES vgc_ ${VGC_DEPENDENCIES})
+    if(WRAPS_TARGET_DEPENDENCIES)
+        add_dependencies(${WRAPS_TARGET} ${WRAPS_TARGET_DEPENDENCIES})
+    endif()
 
 endfunction()
 
@@ -235,15 +279,32 @@ endfunction()
 #   <vgc-build-dir>/<config>/python
 #
 function(vgc_test_library LIB_NAME)
-    message(STATUS "vgc::${LIB_NAME} tests")
+    set(BASE_TARGET         vgc_${LIB_NAME})
+    set(LIB_TARGET          vgc_${LIB_NAME}_lib)
+    set(WRAPS_TARGET        vgc_${LIB_NAME}_wraps)
+    set(CHECK_TARGET        vgc_${LIB_NAME}_check)
+    set(TESTS_TARGET        vgc_${LIB_NAME}_tests)
+    set(CPP_TESTS_TARGET    vgc_${LIB_NAME}_cpp_tests)
+    set(PYTHON_TESTS_TARGET vgc_${LIB_NAME}_python_tests)
+    message(STATUS ${TESTS_TARGET})
 
     set(options "")
     set(oneValueArgs "")
     set(multiValueArgs CPP_TESTS PYTHON_TESTS)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
-    # Add C++ tests
-    vgc_prepend_(LIB_TARGET vgc_lib_ ${LIB_NAME})
+    # Create target vgc_<libname>_tests
+    add_custom_target(${TESTS_TARGET} SOURCES ${ARG_CPP_TESTS} ${ARG_PYTHON_TESTS})
+    set_target_properties(${TESTS_TARGET} PROPERTIES FOLDER libs/${LIB_NAME}/tests)
+    add_dependencies(tests ${TESTS_TARGET})
+
+    # C++ tests
+    if(ARG_CPP_TESTS) # If there is at least one C++ test
+        # Create target vgc_<libname>_cpp_tests
+        add_custom_target(${CPP_TESTS_TARGET} SOURCES ${ARG_CPP_TESTS})
+        set_target_properties(${CPP_TESTS_TARGET} PROPERTIES FOLDER libs/${LIB_NAME}/tests)
+        add_dependencies(${TESTS_TARGET} ${CPP_TESTS_TARGET})
+    endif()
     foreach(FILENAME ${ARG_CPP_TESTS})
         string(REPLACE "." "_" FILENAME_WITHOUT_DOT ${FILENAME})
         set(TEST_TARGET vgc_${LIB_NAME}_${FILENAME})
@@ -266,11 +327,11 @@ function(vgc_test_library LIB_NAME)
         endif()
         add_executable(${EXE_TARGET} EXCLUDE_FROM_ALL ${FILENAME})
         target_link_libraries(${EXE_TARGET} PRIVATE ${LIB_TARGET} gtest)
-        add_dependencies(tests ${EXE_TARGET})
+        add_dependencies(${CPP_TESTS_TARGET} ${EXE_TARGET})
         set_target_properties(${EXE_TARGET} PROPERTIES
             OUTPUT_NAME ${EXE_TARGET_OUTPUT_NAME}
             RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/${TEST_SUBFOLDER}
-            FOLDER libs/${LIB_NAME}/tests
+            FOLDER libs/${LIB_NAME}/tests/cpp_tests
         )
         if(WIN32)            
             add_test(
@@ -294,32 +355,59 @@ function(vgc_test_library LIB_NAME)
         )
     endforeach()
 
-    # Add python tests
-    foreach(PYTHON_TEST_FILENAME ${ARG_PYTHON_TESTS})
-        set(PYTHON_TEST_TARGET_NAME vgc_${LIB_NAME}_${PYTHON_TEST_FILENAME})
+    # Python tests
+    if(ARG_PYTHON_TESTS) # If there is at least one C++ test
+        # Create target vgc_<libname>_python_tests and add the python files
+        # as SOURCES to make them show up on IDEs
+        add_custom_target(${PYTHON_TESTS_TARGET} SOURCES ${ARG_PYTHON_TESTS})
+        set_target_properties(${PYTHON_TESTS_TARGET} PROPERTIES FOLDER libs/${LIB_NAME}/tests)
+        add_dependencies(${TESTS_TARGET} ${PYTHON_TESTS_TARGET})
+        add_dependencies(${PYTHON_TESTS_TARGET} ${BASE_TARGET})
+    endif()
+    foreach(FILENAME ${ARG_PYTHON_TESTS})
+        set(TEST_TARGET vgc_${LIB_NAME}_${FILENAME})
         if(WIN32)
             add_test(
-                NAME ${PYTHON_TEST_TARGET_NAME}
-                COMMAND ${CMAKE_BINARY_DIR}/$<CONFIG>/bin/python.exe ${CMAKE_CURRENT_SOURCE_DIR}/${PYTHON_TEST_FILENAME} -v
+                NAME ${TEST_TARGET}
+                COMMAND ${CMAKE_BINARY_DIR}/$<CONFIG>/bin/python.exe ${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME} -v
                 WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             )
-            set_tests_properties(${PYTHON_TEST_TARGET_NAME} PROPERTIES
+            set_tests_properties(${TEST_TARGET} PROPERTIES
                 ENVIRONMENT "PATH=%PATH%\;${CMAKE_BINARY_DIR}/$<CONFIG>/bin"
             )
         else()
             add_test(
-                NAME ${PYTHON_TEST_TARGET_NAME}
-                COMMAND ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/${PYTHON_TEST_FILENAME} -v
+                NAME ${TEST_TARGET}
+                COMMAND ${Python_EXECUTABLE} ${CMAKE_CURRENT_SOURCE_DIR}/${FILENAME} -v
                 WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             )
-            set_tests_properties(${PYTHON_TEST_TARGET_NAME} PROPERTIES
+            set_tests_properties(${TEST_TARGET} PROPERTIES
                 ENVIRONMENT PYTHONPATH=${CMAKE_BINARY_DIR}/$<CONFIG>/python:$ENV{PYTHONPATH}
             )
         endif()
-        set_property(TEST ${PYTHON_TEST_TARGET_NAME} APPEND PROPERTY
+        set_property(TEST ${TEST_TARGET} APPEND PROPERTY
             ENVIRONMENT VGCBASEPATH=${CMAKE_BINARY_DIR}/$<CONFIG>
         )
     endforeach()
+
+    # Create a target vgc_<libname>_check that builds and runs all the tests in
+    # this lib, re-building test dependencies as needed.
+    if (CMAKE_CONFIGURATION_TYPES)
+        add_custom_target(${CHECK_TARGET} SOURCES ${ARG_CPP_TESTS} ${ARG_PYTHON_TESTS}
+            COMMAND ${CMAKE_CTEST_COMMAND}
+                --force-new-ctest-process --output-on-failure
+                --build-config "$<CONFIGURATION>"
+                -R vgc_${LIB_NAME}
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    else()
+        add_custom_target(${CHECK_TARGET} SOURCES ${ARG_CPP_TESTS} ${ARG_PYTHON_TESTS}
+            COMMAND ${CMAKE_CTEST_COMMAND}
+                --force-new-ctest-process --output-on-failure
+                -R vgc_${LIB_NAME}
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR})
+    endif()
+    set_target_properties(${CHECK_TARGET} PROPERTIES FOLDER libs/${LIB_NAME})
+    add_dependencies(${CHECK_TARGET} ${TESTS_TARGET})
 
 endfunction()
 
@@ -339,33 +427,36 @@ endfunction()
 # )
 #
 function(vgc_add_app APP_NAME)
-    message(STATUS "vgc${APP_NAME}")
+    set(APP_TARGET        vgc_${APP_NAME}_app)
+    set(RESOURCES_TARGET  vgc_${APP_NAME}_resources)
+    message(STATUS ${APP_TARGET})
 
     set(options "")
     set(oneValueArgs "")
     set(multiValueArgs THIRD_DEPENDENCIES VGC_DEPENDENCIES CPP_FILES COMPILE_DEFINITIONS RESOURCE_FILES)
     cmake_parse_arguments(ARG "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
-
-    # Prepend APP_NAME with "vgc_app_" to get target name.
-    vgc_prepend_(TARGET_NAME vgc_app_ ${APP_NAME})
-
+    
     # Add executable
-    add_executable(${TARGET_NAME} WIN32 ${ARG_CPP_FILES})
+    add_executable(${APP_TARGET} WIN32 ${ARG_CPP_FILES})
 
-    # VGC dependencies
-    vgc_prepend_(VGC_DEPENDENCIES vgc_lib_ ${ARG_VGC_DEPENDENCIES})
-    target_link_libraries(${TARGET_NAME} PRIVATE ${VGC_DEPENDENCIES})
+    # VGC dependencies. We need to:
+    # - link to all dependent libs
+    # - build all dependent Python wrappers (the app requires them at runtime)
+    vgc_add_prefix_suffix_(VGC_LIB_DEPENDENCIES vgc_ "${ARG_VGC_DEPENDENCIES}" _lib)
+    vgc_prepend_(APP_TARGET_DEPENDENCIES vgc_ ${ARG_VGC_DEPENDENCIES})
+    target_link_libraries(${APP_TARGET} PRIVATE ${VGC_LIB_DEPENDENCIES})
+    add_dependencies(${APP_TARGET} ${APP_TARGET_DEPENDENCIES})
 
     # Third-party dependencies
-    target_link_libraries(${TARGET_NAME} PRIVATE ${ARG_THIRD_DEPENDENCIES})
+    target_link_libraries(${APP_TARGET} PRIVATE ${ARG_THIRD_DEPENDENCIES})
 
     # Compile definitions, that is, values given to preprocessor variables
-    target_compile_definitions(${TARGET_NAME} PRIVATE ${ARG_COMPILE_DEFINITIONS})
+    target_compile_definitions(${APP_TARGET} PRIVATE ${ARG_COMPILE_DEFINITIONS})
 
     # Set the output name. Example (for now): vgcillustration
     # Under Windows, we may want to call it "VGC_Illustration_2020.exe"
     # Under Linux, we may want to call it vgc-illustration-2020
-    set_target_properties(${TARGET_NAME}
+    set_target_properties(${APP_TARGET}
         PROPERTIES
             OUTPUT_NAME vgc${APP_NAME}
             RUNTIME_OUTPUT_DIRECTORY ${CMAKE_BINARY_DIR}/$<CONFIG>/bin
@@ -384,21 +475,25 @@ function(vgc_add_app APP_NAME)
     )
 
     # Update build copy of resources whenever necessary
-    add_custom_target(${TARGET_NAME}_resources ALL
-        VERBATIM
-        DEPENDS ${RESOURCES_TXT}
-        COMMAND ${Python_EXECUTABLE}
-            "${CMAKE_SOURCE_DIR}/tools/copy_resources.py"
-            "${CMAKE_SOURCE_DIR}"
-            "${CMAKE_BINARY_DIR}"
-            "$<CONFIG>"
-            "app"
-            "${APP_NAME}"
-    )
-    set_target_properties(${TARGET_NAME}_resources
-        PROPERTIES
-            FOLDER apps/${APP_NAME}
-    )
+    if(ARG_RESOURCE_FILES)
+        add_custom_target(${RESOURCES_TARGET}
+            VERBATIM
+            SOURCES ${ARG_RESOURCE_FILES}
+            DEPENDS ${RESOURCES_TXT}
+            COMMAND ${Python_EXECUTABLE}
+                "${CMAKE_SOURCE_DIR}/tools/copy_resources.py"
+                "${CMAKE_SOURCE_DIR}"
+                "${CMAKE_BINARY_DIR}"
+                "$<CONFIG>"
+                "app"
+                "${APP_NAME}"
+        )
+        set_target_properties(${RESOURCES_TARGET}
+            PROPERTIES
+                FOLDER apps/${APP_NAME}
+        )
+        add_dependencies(${APP_TARGET} ${RESOURCES_TARGET})
+    endif()    
 
     if(WIN32)
         # Run windeployqt to copy all required Qt dependencies in the bin
@@ -426,7 +521,7 @@ function(vgc_add_app APP_NAME)
         # example we could implement a way to at least automatically re-run
         # windeployqt when a CMake file changes.
         #
-        add_custom_command(TARGET ${TARGET_NAME} POST_BUILD
+        add_custom_command(TARGET ${APP_TARGET} POST_BUILD
             COMMAND if not exist ${CMAKE_BINARY_DIR}/$<CONFIG>/bin/Qt*.dll
                 ${VGC_QT_ROOT}/bin/windeployqt.exe
                 ${CMAKE_BINARY_DIR}/$<CONFIG>/bin/vgc${APP_NAME}.exe
@@ -441,7 +536,7 @@ function(vgc_add_app APP_NAME)
         # Add dependency to copy_python so that pythonXY.dll is copied to the 
         # build folder when building the app.
         #
-        add_dependencies(${TARGET_NAME} copy_python)
+        add_dependencies(${APP_TARGET} copy_python)
     endif()
 
 endfunction()
