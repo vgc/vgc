e77a06e46679bdc0a09595dc326b4c1f562a1191
Even ENTEM <even.entem@gmail.com>
2022-05-05 10:38:50 +0200

    Add missing include

--- libs/vgc/core/internal/signal.cpp
+++ libs/vgc/core/internal/signal.cpp
@@ -14,6 +14,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#include <unordered_map>
+
 #include <vgc/core/object.h>
 
 namespace vgc::core::internal {
