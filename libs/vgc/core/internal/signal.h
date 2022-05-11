92a9befe7d3067423bd9e1fc63d57cbf5a22355a
Even ENTEM <even.entem@gmail.com>
2022-05-11 16:44:51 +0200

    Set SignalTransmitter default constructor deleted

--- libs/vgc/core/internal/signal.h
+++ libs/vgc/core/internal/signal.h
@@ -413,7 +413,7 @@ public:
     using SlotWrapperSig = void(const TransmitArgs&);
     using SlotWrapper = std::function<SlotWrapperSig>;
 
-    //SignalTransmitter() = default;
+    SignalTransmitter() = delete;
 
     template<typename TSlotWrapper, Requires<isFunctor<RemoveCVRef<TSlotWrapper>>> = true>
     SignalTransmitter(TSlotWrapper&& wrapper, bool isNative = false) :
