00846871a12f1159a8dc07fc901d06206659b2ac
Even ENTEM <even.entem@gmail.com>
2022-08-09 12:08:18 +0200

    Add Colorf

--- libs/vgc/core/color.h
+++ libs/vgc/core/color.h
@@ -232,6 +232,214 @@ private:
     double data_[4];
 };
 
+/// \class vgc::core::Colorf
+/// \brief Color + alpha represented as RBGA with single-precision floats.
+///
+class VGC_CORE_API Colorf
+{
+public:
+    /// Creates an uninitialized Colorf.
+    ///
+    Colorf() {}
+
+    /// Creates a Colorf from the given HSL values.
+    ///
+    /// ```cpp
+    /// core::Colorf c = core::Colorf::hsl(270, 0.6, 0.7);
+    /// ```
+    ///
+    /// The hue should be given in degrees and implicitly wraps around (e.g.,
+    /// adding or substrating any multiple of 360 doesn't change the returned
+    /// color), and the saturation and lightness should be given in [0, 1] and
+    /// are implicitly clamped to this range.
+    ///
+    static Colorf hsl(float h, float s,  float l);
+
+    /// Creates a Colorf initialized with the given r, g, b in [0, 1]. The alpha
+    /// channel is set to 1.0.
+    ///
+    Colorf(float r, float g,  float b) : data_{r, g, b, 1.0} {}
+
+    /// Creates a Colorf initialized with the given r, g, b, a in [0, 1].
+    ///
+    Colorf(float r, float g,  float b, float a) : data_{r, g, b, a} {}
+
+    /// Accesses the i-th channel of the Colorf.
+    ///
+    const float& operator[](int i) const { return data_[i]; }
+
+    /// Mutates the i-th channel of the Colorf.
+    ///
+    float& operator[](int i) { return data_[i]; }
+
+    /// Accesses the red channel of the Colorf.
+    ///
+    float r() const { return data_[0]; }
+
+    /// Accesses the green channel of the Colorf.
+    ///
+    float g() const { return data_[1]; }
+
+    /// Accesses the blue channel of the Colorf.
+    ///
+    float b() const { return data_[2]; }
+
+    /// Accesses the alpha channel of the Colorf.
+    ///
+    float a() const { return data_[3]; }
+
+    /// Mutates the red channel of the Colorf.
+    ///
+    void setR(float r) { data_[0] = r; }
+
+    /// Mutates the green channel of the Colorf.
+    ///
+    void setG(float g) { data_[1] = g; }
+
+    /// Mutates the blue channel of the Colorf.
+    ///
+    void setB(float b) { data_[2] = b; }
+
+    /// Mutates the alpha channel of the Colorf.
+    ///
+    void setA(float a) { data_[3] = a; }
+
+    /// Adds in-place the \p other Colorf to this Colorf.
+    /// This is a component-wise addition of all channels, including the alpha
+    /// channel.
+    ///
+    Colorf& operator+=(const Colorf& other) {
+        data_[0] += other[0];
+        data_[1] += other[1];
+        data_[2] += other[2];
+        data_[3] += other[3];
+        return *this;
+    }
+
+    /// Returns the addition of the Colorf \p c1 and the Colorf \p c2.
+    /// This is a component-wise addition of all channels, including the alpha
+    /// channel.
+    ///
+    friend Colorf operator+(const Colorf& c1, const Colorf& c2) {
+        return Colorf(c1) += c2;
+    }
+
+    /// Substracts in-place the \p other Colorf to this Colorf.
+    /// This is a component-wise substraction of all channels, including the
+    /// alpha channel.
+    ///
+    Colorf& operator-=(const Colorf& other) {
+        data_[0] -= other[0];
+        data_[1] -= other[1];
+        data_[2] -= other[2];
+        data_[3] -= other[3];
+        return *this;
+    }
+
+    /// Returns the substraction of the Colorf \p c1 and the Colorf \p c2.
+    /// This is a component-wise substraction of all channels, including the
+    /// alpha channel.
+    ///
+    friend Colorf operator-(const Colorf& c1, const Colorf& c2) {
+        return Colorf(c1) -= c2;
+    }
+
+    /// Multiplies in-place this Colorf by the given scalar \p s.
+    /// This multiplies all channels, including the alpha channel.
+    ///
+    Colorf& operator*=(float s) {
+        data_[0] *= s;
+        data_[1] *= s;
+        data_[2] *= s;
+        data_[3] *= s;
+        return *this;
+    }
+
+    /// Returns the multiplication of this Colorf by the given scalar \p s.
+    /// This multiplies all channels, including the alpha channel.
+    ///
+    Colorf operator*(float s) const {
+        return Colorf(*this) *= s;
+    }
+
+    /// Returns the multiplication of the scalar \p s with the Colorf \p c.
+    /// This multiplies all channels, including the alpha channel.
+    ///
+    friend Colorf operator*(float s, const Colorf& c) {
+        return c * s;
+    }
+
+    /// Divides in-place this Colorf by the given scalar \p s.
+    /// This divides all channels, including the alpha channel.
+    ///
+    Colorf& operator/=(float s) {
+        data_[0] /= s;
+        data_[1] /= s;
+        data_[2] /= s;
+        data_[3] /= s;
+        return *this;
+    }
+
+    /// Returns the division of this Colorf by the given scalar \p s.
+    /// This divides all channels, including the alpha channel.
+    ///
+    Colorf operator/(float s) const {
+        return Colorf(*this) /= s;
+    }
+
+    /// Returns whether the two given Colorf \p c1 and \p c2 are equal.
+    ///
+    friend bool operator==(const Colorf& c1, const Colorf& c2) {
+        return c1.data_[0] == c2.data_[0] &&
+            c1.data_[1] == c2.data_[1] &&
+            c1.data_[2] == c2.data_[2] &&
+            c1.data_[3] == c2.data_[3];
+    }
+
+    /// Returns whether the two given Colorf \p c1 and \p c2 are different.
+    ///
+    friend bool operator!=(const Colorf& c1, const Colorf& c2) {
+        return !(c1 == c2);
+    }
+
+    /// Compares the two Colorf \p c1 and \p c2 using the lexicographic
+    /// order.
+    ///
+    friend bool operator<(const Colorf& c1, const Colorf& c2) {
+        return ( (c1.data_[0] < c2.data_[0]) ||
+                (!(c2.data_[0] < c1.data_[0]) &&
+                 ( (c1.data_[1] < c2.data_[1]) ||
+                  (!(c2.data_[1] < c1.data_[1]) &&
+                   ( (c1.data_[2] < c2.data_[2]) ||
+                    (!(c2.data_[2] < c1.data_[2]) &&
+                     ( (c1.data_[3] < c2.data_[3]))))))));
+    }
+
+    /// Compares the two Colorf \p c1 and \p c2 using the lexicographic
+    /// order.
+    ///
+    friend bool operator<=(const Colorf& c1, const Colorf& c2) {
+        return !(c2 < c1);
+    }
+
+    /// Compares the two Colorf \p c1 and \p c2 using the lexicographic
+    /// order.
+    ///
+    friend bool operator>(const Colorf& c1, const Colorf& c2) {
+        return c2 < c1;
+    }
+
+    /// Compares the two Colorf \p c1 and \p c2 using the lexicographic
+    /// order.
+    ///
+    friend bool operator>=(const Colorf& c1, const Colorf& c2) {
+        return !(c1 < c2);
+    }
+
+private:
+    float data_[4];
+};
+
 /// Writes the given Color to the output stream.
 ///
 /// The written string is a valid CSS Color Module Level 3, see:
