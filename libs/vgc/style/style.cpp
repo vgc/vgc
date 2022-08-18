b1bf3121c173f5ae85199570a99dfe298ed16dac
Boris Dalstein <dalboris@gmail.com>
2022-08-18 17:07:23 +0200

    Revert dummy change

--- libs/vgc/style/style.cpp
+++ libs/vgc/style/style.cpp
@@ -674,7 +674,6 @@ StyleSelector::StyleSelector(core::Array<StyleSelectorItem>&& items)
     , specificity_(0) {
 
     // Compute specificity
-    //
     for (StyleSelectorItem& item : items_) {
         if (item.type() == StyleSelectorItemType::ClassSelector) {
             ++specificity_;
