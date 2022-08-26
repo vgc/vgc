ac4f201175886c57f5bee15ad68e08393750315b
Even ENTEM <even.entem@gmail.com>
2022-08-26 19:23:18 +0200

    Implement Margins class (#457) (#843)

---
+++ libs/vgc/ui/margins.cpp
@@ -0,0 +1,23 @@
+// Copyright 2022 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vgc/ui/margins.h>
+
+namespace vgc::ui {
+
+// all definitions are inline
+
+} // namespace vgc::ui
