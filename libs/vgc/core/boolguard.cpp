deaefd28b78bd105291948fc4545c604d58cdebd
Boris Dalstein <dalboris@gmail.com>
2023-04-11 14:49:29 +0200

    Test adding a .cpp to fix unresolved symbol

---
+++ libs/vgc/core/boolguard.cpp
@@ -0,0 +1,20 @@
+// Copyright 2023 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vgc/core/boolguard.h>
+
+// If you implement methods of BoolGuard here, don't forget to
+// add VGC_CORE_API to the class and remove this paragraph.
