48a567f8be21766eadd3ed30995ac2b8d5254091
Boris Dalstein <dalboris@gmail.com>
2023-04-14 13:07:19 +0200

    Remove unused header

--- libs/vgc/ui/panelarea.cpp
+++ libs/vgc/ui/panelarea.cpp
@@ -14,8 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <list>
-
 #include <vgc/ui/panelarea.h>
 
 #include <vgc/core/colors.h>
