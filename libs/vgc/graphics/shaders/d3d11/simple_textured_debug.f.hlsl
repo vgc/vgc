73dbd72a7daf0f6b8e0b36fb10f6fb3659356d89
Even ENTEM <even.entem@gmail.com>
2023-04-21 16:14:56 +0200

    Implement alternative visualizations in SimpleTextureDebug HLSL shader (#1201) (#1229)

--- libs/vgc/graphics/shaders/d3d11/simple_textured_debug.f.hlsl
+++ libs/vgc/graphics/shaders/d3d11/simple_textured_debug.f.hlsl
@@ -13,14 +13,29 @@ float avg(float3 x) {
 float4 main(PS_INPUT input)
     : SV_Target {
 
-    const float square_size = 4;
+    // gradient in V (-1:red, 0: black, 1:green) with 0.5 opacity
+    //if (input.uv.y > 0) {
+    //    return float4(input.uv.y, 0.f, 0.f, 0.5f);
+    //}
+    //else {
+    //    return float4(0.f, 0.f, -input.uv.y, 0.5f);
+    //}
+
+    // smooth brush test (easings.net/#easeInOutQuad)
+    //float x = 1.f - abs(input.uv.y);
+    //float k = -2 * x + 2;
+    //float ca = x < 0.5 ? 2 * x * x : 1 - k * k / 2;
+    //return float4(input.col.rgb, input.col.a * ca);
+
+    const float square_size = 0.05;
     const float m = square_size * 2;
     float2 uvm = (m + ((input.uv + 2.0) % m)) % m;
 
     if ((uvm.x < square_size) == (uvm.y < square_size)) {
         return float4(0, 0, 0, input.col.a * 2);
     }
-    float3 c = input.col.rgb;// lerp(input.col.rgb, input.col.gbr, input.uv.x / 160.0 % 0.5);
+    float3 c =
+        input.col.rgb; // lerp(input.col.rgb, input.col.gbr, input.uv.x / 160.0 % 0.5);
     if (input.uv.y < -0.1) {
         c += (round(avg(c)) * 2 - 1) * 0.5;
     }
