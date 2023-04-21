856ec1eed91eb986b1b6e88bab8f9cee850c1c16
Boris Dalstein <dalboris@gmail.com>
2023-04-21 11:27:16 +0200

    Implement Setting, SettingEdit, NumberSetting, NumberSettingEdit (#1199) (#1226)

---
+++ libs/vgc/ui/numbersetting.cpp
@@ -0,0 +1,162 @@
+// Copyright 2023 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vgc/ui/numbersetting.h>
+
+#include <vgc/ui/settings.h>
+
+namespace vgc::ui {
+
+NumberSetting::NumberSetting(
+    Settings* settings,
+    std::string_view key,
+    std::string_view label,
+    double defaultValue)
+
+    : Setting(settings, key, label)
+    , defaultValue_(defaultValue) {
+}
+
+NumberSettingPtr NumberSetting::create(
+    Settings* settings,
+    std::string_view key,
+    std::string_view label,
+    double defaultValue) {
+
+    return NumberSettingPtr(new NumberSetting(settings, key, label, defaultValue));
+}
+
+double NumberSetting::value() const {
+
+    // Get currently stored value.
+    //
+    double value_ = settings()->getOrSetDoubleValue(key(), defaultValue_);
+
+    // Clamp and round value to given range and precision.
+    //
+    // Note: we must defer clamping/rounding as we do here, otherwise calling
+    // several setters (min, max, or precision) would apply the constraint at
+    // each step, instead of waiting for all constraints to be set.
+    //
+    // Example:
+    //   stored setting on file = 200
+    //   NumberSetting setting = NumberSetting::create(...);
+    //   settings->setRange(10, 1000);
+    //
+    //  => This would start by calling setMinimum(10), which if not deferred
+    //     would clamp 200 to (10, 100), i.e. change it to 100, before then
+    //     increasing the maximum.
+    //
+    if (!isValueClampedAndRounded_) {
+        double newValue = clampedAndRoundedValue_(value_);
+        isValueClampedAndRounded_ = true;
+        if (value_ != newValue) {
+            settings()->setDoubleValue(key(), newValue);
+            value_ = newValue;
+        }
+    }
+
+    return value_;
+}
+
+void NumberSetting::setValue(double newValue) {
+
+    // Set new value
+    double value_ = value();
+    if (value_ == newValue) {
+        return;
+    }
+    newValue = clampedAndRoundedValue_(newValue);
+    if (value_ == newValue) {
+        return;
+    }
+    settings()->setDoubleValue(key(), newValue);
+
+    // Emit signal
+    valueChanged().emit(value_);
+}
+
+void NumberSetting::setStep(double step) {
+    step_ = step;
+}
+
+void NumberSetting::setMinimum(double min) {
+
+    // Set new minimum
+    if (minimum_ == min) {
+        return;
+    }
+    double newMin = roundedValue_(min);
+    if (minimum_ == newMin) {
+        return;
+    }
+    minimum_ = newMin;
+
+    // Ensure range is valid (min <= max)
+    if (maximum_ < minimum_) {
+        maximum_ = minimum_;
+    }
+
+    // Fit value in new range
+    clampAndRound_();
+}
+
+void NumberSetting::setMaximum(double max) {
+
+    // Set new maximum
+    if (maximum_ == max) {
+        return;
+    }
+    double newMax = roundedValue_(max);
+    if (maximum_ == newMax) {
+        return;
+    }
+    maximum_ = newMax;
+
+    // Ensure range is valid (min <= max)
+    if (maximum_ < minimum_) {
+        minimum_ = maximum_;
+    }
+
+    // Fit value in new range
+    clampAndRound_();
+}
+
+void NumberSetting::setPrecision(core::Precision precision) {
+    if (precision_ == precision) {
+        return;
+    }
+    precision_ = precision;
+    setMinimum(roundedValue_(minimum_));
+    setMaximum(roundedValue_(maximum_));
+    clampAndRound_();
+}
+
+void NumberSetting::clampAndRound_() {
+    defaultValue_ = clampedAndRoundedValue_(defaultValue_);
+    isValueClampedAndRounded_ = false;
+}
+
+double NumberSetting::roundedValue_(double v) const {
+    return vgc::core::round(v, precision_);
+}
+
+double NumberSetting::clampedAndRoundedValue_(double v) const {
+    double res = core::clamp(v, minimum(), maximum());
+    return roundedValue_(res);
+}
+
+} // namespace vgc::ui
---
+++ libs/vgc/ui/setting.h
@@ -0,0 +1,73 @@
+// Copyright 2023 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VGC_UI_SETTING_H
+#define VGC_UI_SETTING_H
+
+#include <vgc/core/object.h>
+#include <vgc/ui/api.h>
+#include <vgc/ui/settings.h>
+
+namespace vgc::ui {
+
+VGC_DECLARE_OBJECT(Setting);
+VGC_DECLARE_OBJECT(Settings);
+
+/// \class vgc::ui::Setting
+/// \brief Represents an individual setting in a given `Settings`.
+///
+class VGC_UI_API Setting : public core::Object {
+private:
+    VGC_OBJECT(Setting, core::Object)
+
+protected:
+    Setting(Settings* settings, std::string_view key, std::string_view label);
+
+public:
+    /// Creates a `Setting`.
+    ///
+    static SettingPtr
+    create(Settings* settings, std::string_view key, std::string_view label);
+
+    /// Returns the `Settings` object this `SettingEdit` is synced to.
+    ///
+    Settings* settings() const {
+        return settings_.get();
+    }
+
+    /// Returns the key corresponding to the value that his this `SettingEdit`
+    /// controls.
+    ///
+    std::string_view key() const {
+        return key_;
+    }
+
+    /// Returns the label of the setting, that is, a short human-readable
+    /// description for display purposes in the UI.
+    ///
+    std::string_view label() const {
+        return label_;
+    }
+
+private:
+    SettingsPtr settings_;
+    std::string key_;
+    std::string label_;
+};
+
+} // namespace vgc::ui
+
+#endif // VGC_UI_SETTING_H
