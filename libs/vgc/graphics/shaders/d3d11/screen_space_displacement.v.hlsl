59792eb25982d8f5cebaa3da69958449edce16d7
Even ENTEM <even.entem@gmail.com>
2023-04-24 17:36:18 +0200

    Allow null displacement in screen-space displacement shader (#1201) (#1246)

--- libs/vgc/graphics/shaders/d3d11/screen_space_displacement.v.hlsl
+++ libs/vgc/graphics/shaders/d3d11/screen_space_displacement.v.hlsl
@@ -27,16 +27,19 @@ PS_INPUT main(VS_INPUT input) {
     PS_INPUT output;
 
     float4 viewPos = mul(viewMatrix, float4(input.pos + input.ipos, 0.f, 1.f));
-    float dispMag = length(input.disp) * input.offset;
-    float2 dispDir = input.disp;
-    // input.rotate is "Rot" and is a "float" boolean to enable the rotation of the displacement by the view matrix.
-    if (input.rotate > 0) {
-        dispDir = mul(viewMatrix, float4(dispDir, 0.f, 0.f)).xy;
-    }
-    dispDir = normalize(dispDir);
-
     output.pos = mul(projMatrix, viewPos);
-    output.pos.xy += dispMag * dispDir / float2(0.5 * viewport.z, -0.5 * viewport.w);
+
+    float2 dispDir = input.disp;
+    if (any(dispDir != 0)) {
+        float dispMag = length(input.disp) * input.offset;
+        // input.rotate is "Rot" and is a "float" boolean to enable the rotation of the displacement by the view matrix.
+        if (input.rotate > 0) {
+            dispDir = mul(viewMatrix, float4(dispDir, 0.f, 0.f)).xy;
+        }
+        dispDir = normalize(dispDir);
+        output.pos.xy += dispMag * dispDir / float2(0.5 * viewport.z, -0.5 * viewport.w);
+    }
+
     output.clipPos = output.pos;
     output.col = input.col;
 
