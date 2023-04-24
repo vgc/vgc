7ce7bad89f14995a37cd16a1f2f5b36d7182bc1d
Even ENTEM <even.entem@gmail.com>
2023-04-24 16:15:40 +0200

    Allow null displacement in screen-space displacement shader (#1201)

--- libs/vgc/graphics/shaders/opengl/screen_space_displacement.v.glsl
+++ libs/vgc/graphics/shaders/opengl/screen_space_displacement.v.glsl
@@ -19,15 +19,18 @@ out vec4 fCol;
 
 void main() {
     vec4 viewPos = view * vec4(pos + ipos, 0.f, 1.f);
-    float dispMag = length(disp) * offset;
-    vec2 dispDir = disp;
-    // rotate is "Rot" and is a "float" boolean to enable the rotation of the displacement by the view matrix.
-    if (rotate > 0) {
-        dispDir = (view * vec4(dispDir, 0.f, 0.f)).xy;
-    }
-    dispDir = normalize(dispDir);
+     gl_Position = proj * viewPos;
 
-    gl_Position = proj * viewPos;
-    gl_Position.xy += dispMag * dispDir / vec2(0.5 * viewport.z, -0.5 * viewport.w);
+    vec2 dispDir = disp;
+    if (dispDir != vec2(0, 0)) {
+        float dispMag = length(disp) * offset;
+        // rotate is "Rot" and is a "float" boolean to enable the rotation of the displacement by the view matrix.
+        if (rotate > 0) {
+            dispDir = (view * vec4(dispDir, 0.f, 0.f)).xy;
+        }
+        dispDir = normalize(dispDir);
+        gl_Position.xy += dispMag * dispDir / vec2(0.5 * viewport.z, -0.5 * viewport.w);
+    }
+    
     fCol = col;
 }
