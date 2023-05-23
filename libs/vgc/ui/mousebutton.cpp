ef024fe4f061940000a837f8bc3a89e97ae3e2f1
Boris Dalstein <dalboris@gmail.com>
2023-05-23 14:50:56 +0200

    Add MouseButton enum strings; Move MouseButton implementation to dedicated .h/.cpp file (#994) (#1320)

---
+++ libs/vgc/ui/mousebutton.cpp
@@ -0,0 +1,52 @@
+// Copyright 2023 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <vgc/ui/mousebutton.h>
+
+namespace vgc::ui {
+
+VGC_DEFINE_ENUM(
+    MouseButton,
+    (None, "None"),
+    (Left, "Left"),
+    (Right, "Right"),
+    (Middle, "Middle"),
+    (Extra1, "Extra 1"),
+    (Extra2, "Extra 2"),
+    (Extra3, "Extra 3"),
+    (Extra4, "Extra 4"),
+    (Extra5, "Extra 5"),
+    (Extra6, "Extra 6"),
+    (Extra7, "Extra 7"),
+    (Extra8, "Extra 8"),
+    (Extra9, "Extra 9"),
+    (Extra10, "Extra 10"),
+    (Extra11, "Extra 11"),
+    (Extra12, "Extra 12"),
+    (Extra13, "Extra 13"),
+    (Extra14, "Extra 14"),
+    (Extra15, "Extra 15"),
+    (Extra16, "Extra 16"),
+    (Extra17, "Extra 17"),
+    (Extra18, "Extra 18"),
+    (Extra19, "Extra 19"),
+    (Extra20, "Extra 20"),
+    (Extra21, "Extra 21"),
+    (Extra22, "Extra 22"),
+    (Extra23, "Extra 23"),
+    (Extra24, "Extra 24"))
+
+} // namespace vgc::ui
--- libs/vgc/ui/mouseevent.h
+++ libs/vgc/ui/mouseevent.h
@@ -17,55 +17,14 @@
 #ifndef VGC_UI_MOUSEEVENT_H
 #define VGC_UI_MOUSEEVENT_H
 
-#include <vgc/core/arithmetic.h>
-
-#include <vgc/core/flags.h>
 #include <vgc/geometry/vec2f.h>
 #include <vgc/ui/event.h>
 #include <vgc/ui/modifierkey.h>
+#include <vgc/ui/mousebutton.h>
 
 namespace vgc::ui {
 
 VGC_DECLARE_OBJECT(MouseEvent);
-
-// clang-format off
-
-/// \class vgc::ui::MouseButton
-/// \brief Enumeration of all possible mouse buttons
-///
-enum class MouseButton : UInt32 {
-    None    = 0x00000000,
-    Left    = 0x00000001,
-    Right   = 0x00000002,
-    Middle  = 0x00000004,
-    Extra1  = 0x00000008,
-    Extra2  = 0x00000010,
-    Extra3  = 0x00000020,
-    Extra4  = 0x00000040,
-    Extra5  = 0x00000080,
-    Extra6  = 0x00000100,
-    Extra7  = 0x00000200,
-    Extra8  = 0x00000400,
-    Extra9  = 0x00000800,
-    Extra10 = 0x00001000,
-    Extra11 = 0x00002000,
-    Extra12 = 0x00004000,
-    Extra13 = 0x00008000,
-    Extra14 = 0x00010000,
-    Extra15 = 0x00020000,
-    Extra16 = 0x00040000,
-    Extra17 = 0x00080000,
-    Extra18 = 0x00100000,
-    Extra19 = 0x00200000,
-    Extra20 = 0x00400000,
-    Extra21 = 0x00800000,
-    Extra22 = 0x01000000,
-    Extra23 = 0x02000000,
-    Extra24 = 0x04000000
-};
-VGC_DEFINE_FLAGS(MouseButtons, MouseButton)
-
-// clang-format on
 
 /// \class vgc::ui::HoverLockPolicy
 /// \brief Specifies hover-locking behavior.
