69d19dd213b64fe08b598589bdfc0609ab5ac952
Boris Dalstein <dalboris@gmail.com>
2024-07-26 19:30:27 +0200

    Add basic geometry::Polyline skeleton classes (#1819) (#1820)

---
+++ libs/vgc/geometry/polyline2f.h
@@ -0,0 +1,29 @@
+// Copyright 2024 The VGC Developers
+// See the COPYRIGHT file at the top-level directory of this distribution
+// and at https://github.com/vgc/vgc/blob/master/COPYRIGHT
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef VGC_GEOMETRY_POLYLINE2F_H
+#define VGC_GEOMETRY_POLYLINE2F_H
+
+#include <vgc/geometry/polyline2.h>
+#include <vgc/geometry/vec2f.h>
+
+namespace vgc::geometry {
+
+using Polyline2f = Polyline2<Vec2f>;
+
+} // namespace vgc::geometry
+
+#endif // VGC_GEOMETRY_POLYLINE2F_H
