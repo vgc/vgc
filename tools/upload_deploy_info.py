98f67c67a42f1c53b3d46fb963ce79ce3348f6a1
Boris Dalstein <dalboris@gmail.com>
2024-08-02 14:15:11 +0200

    Minimal working upload_deploy_info.py

--- tools/upload_deploy_info.py
+++ tools/upload_deploy_info.py
@@ -4,99 +4,122 @@ from pathlib import Path
 import argparse
 import json
 import os
+import re
 import sys
 import time
 import urllib.parse
 import urllib.request
 
-# Prints with flush=True by default. Using flush=True tends to behave better and
-# prevent truncating and ordering issues in TravisCI and perhaps other systems
-# which process and redirect the output.
+# Prints with flush=True by default, preventing truncattion/order issues in CI logs.
 #
 def print_(*objects, sep=' ', end='\n', file=sys.stdout, flush=True):
     print(*objects, sep=sep, end=end, file=file, flush=flush)
+
+
+# Returns whether the given 'name' is likely to refer to sensitive information.
+#
+def is_sensitive(name):
+    name = name.lower()
+    for s in ['pass', 'key', 'token']:
+        if s in name:
+            return True
+    return False
+
+
+# Returns a copy of the data but with sensitive information replaced
+# with asterisks.
+#
+def hide_sensitive(data: dict):
+    res = {}
+    for key, value in data.items():
+        if isinstance(value, dict):
+            res[key] = hide_sensitive(v)
+        elif value != "" and is_sensitive(key) :
+            res[key] = '******'
+        else:
+             res[key] = value
+    return res
+
 
 # Makes a POST request to the given URL with the given data.
 # The given data should be a Python dictionary, which this function
 # automatically encodes as JSON. Finally, the JSON response is decoded
 # and returned as a python dictionary.
 #
-def post_json(url, data):
-    databytes = json.dumps(data).encode("utf-8")
-    request = urllib.request.Request(url)
+def post_json(url, urlargs, jsondata):
+
+    jsontext = json.dumps(jsondata)
+    jsonbytes = jsontext.encode("utf-8")
+
+    print_(f"POST {url}")
+    print_(f"  args={hide_sensitive(urlargs)}")
+    print_(f"  json={hide_sensitive(jsondata)}")
+
+    urlWithArgs = url + "?" + urllib.parse.urlencode(urlargs)
+    request = urllib.request.Request(urlWithArgs)
     request.method = "POST"
     request.add_header("Content-Type", "application/json; charset=utf-8")
     request.add_header("Connection", "close")
-    response = urllib.request.urlopen(request, databytes)
-    with urllib.request.urlopen(request, databytes) as response:
+    with urllib.request.urlopen(request, jsonbytes) as response:
         encoding = response.info().get_param("charset") or "utf-8"
-        return json.loads(response.read().decode(encoding))
+        responsedata = json.loads(response.read().decode(encoding))
+        print_(f"  response={hide_sensitive(responsedata)}")
+        print_("  Done.")
+        return responsedata
 
-# Contructs a URL by concatenating the given base url with
-# the given query parameters.
-#
-# Example:
-# urlencode("http://www.example.com", {"key": "hello", "password": "world"})
-#
-# Output:
-# "http://www.example.com?key=hello&password=world"
-#
-def urlencode(url, data):
-    return url + "?" + urllib.parse.urlencode(data)
 
-# Script entry point.
-#
-if __name__ == "__main__":
+def github_upload(version, files):
 
-    # Set verbosity
-    verbose = True
+    urlargs = {}
+    eventName = os.getenv("GITHUB_EVENT_NAME")
 
-    # Parse arguments, and import them into the global namespace
-    parser = argparse.ArgumentParser()
-    parser.add_argument("file", help="path to the info.json file")
-    args = parser.parse_args()
-    globals().update(vars(args))
+    if eventName == "pull_request":
+        ref = os.getenv("GITHUB_REF")
+        assert ref, "Missing GITHUB_REF."
+        pr = ref.split('/')[2]
+        assert pr.isdigit(), "Failed to extract PR number from GITHUB_REF (={ref})."
+        urlargs['key'] = ""
+        urlargs['pr'] =  pr
 
-    file = Path(file)
+    elif eventName == "push":
+        key = os.getenv("VGC_GITHUB_KEY")
+        assert key, "Missing key: cannot upload artifact"
+        urlargs['key'] = key
+        urlargs['pr'] = "false"
+
+    else:
+        print_("Skipping upload as this does not appear to be a pull request or push.")
+        return
+
+    url = "https://webhooks.vgc.io/github"
+    response = post_json(url, urlargs, version)
+    urlargs['releaseId'] = response["releaseId"]
+    for filename in files:
+        urlargs['filename'] = filename
+        urlargs['externalUrl'] = f"https://example.com/{filename}"
+        post_json(url, urlargs, {})
+
+
+def main(args):
+
+    # Get file/dirs
+    file = Path(args.file)
     deployDir = file.parent
 
-    info = json.load(file.read_text())
+    # Parse JSON file
+    info = json.loads(file.read_text())
     version = info['version']
     files = info['files']
 
-    upload = False
+    # Upload deploy info
     if os.getenv("GITHUB_REPOSITORY") == "vgc/vgc":
-        eventName = os.getenv("GITHUB_EVENT_NAME")
-        url = "https://webhooks.vgc.io/github"
-        if eventName == "pull_request":
-            upload = True
-            key = ""
-            ref = os.getenv("GITHUB_REF") # Example: refs/pull/461/merge
-            pr = ref.split('/')[2]        # Example: 461
-        elif eventName == "push":
-            upload = True
-            key = os.getenv("VGC_GITHUB_KEY")
-            assert key, "Missing key: cannot upload artifact"
-            pr = "false"
-        else:
-            upload = False
-    if upload:
-        print_("Uploading commit metadata...")
-        response = post_json(
-            urlencode(url, {
-                "key": key,
-                "pr": pr
-            }), version)
-        print_(" Done.")
-        releaseId = response["releaseId"]
-        allFilesUploaded = True
-        for filename in files:
-            response = post_json(
-                        urlencode(url, {
-                            "key": key,
-                            "pr": pr,
-                            "releaseId": releaseId,
-                            "filename": filename,
-                            "externalUrl": f"https://example.com/{filename}"
-                        }), {
-                    })
+        github_upload(version, files)
+    else:
+        print_("Skipping upload as this does not appear to be an official VGC repository.")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file", help="path to the info.json file")
+    args = parser.parse_args()
+    main(args)
