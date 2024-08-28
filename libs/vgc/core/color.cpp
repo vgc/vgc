a90d8e87eea9b2bee2e78e977bee612fb84e1e67
Boris Dalstein <dalboris@gmail.com>
2024-08-28 13:02:45 +0200

    Remove intentional format failure

--- libs/vgc/core/color.cpp
+++ libs/vgc/core/color.cpp
@@ -24,7 +24,6 @@ namespace {
 
 using NamedColorMap = std::unordered_map<std::string_view, Color>;
 
-
 NamedColorMap generateNamedColorMap() {
     return {
         {"aliceblue", Color::fromRgb8(240, 248, 255)},
